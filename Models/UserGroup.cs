1defb757deeb35cf1f254ce9df907d201f2f2e10
dimitrios <dimitrios@local.local>
2020-11-30 16:40:59 +0200

    Remove user group model enums.

--- Models/UserGroup.cs
+++ Models/UserGroup.cs
@@ -27,18 +27,6 @@ namespace Gizmo.Web.Api.Models
         public string Description { get; set; }
 
         /// <summary>
-        /// The Id of the application profile this user group is associated with.
-        /// </summary>
-        [DataMember]
-        public int? ApplicationGroup { get; set; }
-
-        /// <summary>
-        /// The Id of the security profile this user group is associated with.
-        /// </summary>
-        [DataMember]
-        public int? SecurityProfile { get; set; }
-
-        /// <summary>
         /// Gets or sets bill profile id.
         /// </summary>
         [DataMember]
@@ -48,25 +36,82 @@ namespace Gizmo.Web.Api.Models
         /// Gets or sets required user info.
         /// </summary>
         [DataMember]
-        public UserInfoTypes RequiredUserInfo { get; set; }
+        public RequiredUserInfo RequiredUserInfo { get; set; }
+
+        [DataMember]
+        public bool OverrideApplicationGroup { get; set; }
 
         /// <summary>
-        /// Gets or sets ovverides.
+        /// The Id of the application profile this user group is associated with.
         /// </summary>
         [DataMember]
-        public GroupOverrides Overrides { get; set; }
+        public int? ApplicationGroup { get; set; }
+
+        [DataMember]
+        public bool OverrideSecurityProfile { get; set; }
 
         /// <summary>
-        /// Gets or sets options.
+        /// The Id of the security profile this user group is associated with.
         /// </summary>
         [DataMember]
-        public UserGroupOptionType Options { get; set; }
+        public int? SecurityProfile { get; set; }
+
+        [DataMember]
+        public bool OverrideAgeRating { get; set; }
 
         /// <summary>
-        /// Gets or sets credit limit options.
+        /// Gets or sets if age rating is enabled for the group.
         /// </summary>
         [DataMember]
-        public CreditLimitOptionType CreditLimitOptions { get; set; }
+        public bool IsAgeRatingEnabled { get; set; }
+
+        /// <summary>
+        /// Enable personal storage.
+        /// </summary>
+        [DataMember]
+        public bool EnablePersonalStorage { get; set; }
+
+        /// <summary>
+        /// Hide logout button.
+        /// </summary>
+        [DataMember]
+        public bool HideLogoutButton { get; set; }
+
+        /// <summary>
+        /// Disallow manual login.
+        /// </summary>
+        [DataMember]
+        public bool DisallowManualLogin { get; set; }
+
+        /// <summary>
+        /// Allow guest use.
+        /// </summary>
+        [DataMember]
+        public bool GuestUse { get; set; }
+
+        /// <summary>
+        /// Allow only guest use.
+        /// </summary>
+        [DataMember]
+        public bool GuestUseOnly { get; set; }
+
+        /// <summary>
+        /// Enables or disables personal files.
+        /// </summary>
+        [DataMember]
+        public bool EnablePersonalFiles { get; set; }
+
+        /// <summary>
+        /// Disallow login from manager.
+        /// </summary>
+        [DataMember]
+        public bool DisallowLoginFromManager { get; set; }
+
+        /// <summary>
+        /// Enable per user credit limit.
+        /// </summary>
+        [DataMember]
+        public bool EnablePerUserCreditLimit { get; set; }
 
         /// <summary>
         /// Gets or sets if user group allows negative balance.
@@ -110,17 +155,14 @@ namespace Gizmo.Web.Api.Models
         [DataMember]
         public bool IsDefault { get; set; }
 
-        /// <summary>
-        /// Gets or sets if age rating is enabled for the group.
-        /// </summary>
         [DataMember]
-        public bool IsAgeRatingEnabled { get; set; }
+        public bool DisableTimeOffer { get; set; }
 
-        ///// <summary>
-        ///// Gets or sets user billing option.
-        ///// </summary>
-        //[DataMember]
-        //public BillingOption BillingOptions { get; set; }
+        [DataMember]
+        public bool DisableFixedTime { get; set; }
+
+        [DataMember]
+        public bool DisableDeposit { get; set; }
 
         /// <summary>
         /// Gets or sets user group priority.
