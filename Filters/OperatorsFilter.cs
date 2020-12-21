d42c04377279b644c054293045a12e6f4556cae0
dimitrios <dimitrios@local.local>
2020-12-21 17:25:59 +0200

    Add operators models.

--- Filters/OperatorsFilter.cs
+++ Filters/OperatorsFilter.cs
@@ -4,32 +4,26 @@ using System.Runtime.Serialization;
 namespace Gizmo.Web.Api.Models
 {
     /// <summary>
-    /// Filters that can be applied when searching for users.
+    /// Filters that can be applied when searching for operators.
     /// </summary>
     [Serializable]
     [DataContract]
-    public class UsersFilter : PaginationFilter, IUrlQueryParameters
+    public class OperatorsFilter : PaginationFilter, IUrlQueryParameters
     {
         /// <summary>
-        /// Return users of the specified user group.
-        /// </summary>
-        [DataMember]
-        public int? UserGroupId { get; set; }
-
-        /// <summary>
-        /// Return users with usernames that contain the specified string.
+        /// Return operators with usernames that contain the specified string.
         /// </summary>
         [DataMember]
         public string Username { get; set; }
 
         /// <summary>
-        /// Return disabled users.
+        /// Return disabled operators.
         /// </summary>
         [DataMember]
         public bool? IsDisabled { get; set; }
 
         /// <summary>
-        /// Return deleted users.
+        /// Return deleted operators.
         /// </summary>
         [DataMember]
         public bool? IsDeleted { get; set; }
---
+++ Models/OperatorModelUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Operator.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class OperatorModelUpdate : OperatorModelBase, IEntityBase, IUrlQueryParameters
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the object.
+        /// </summary>
+        [DataMember]
+        [Required]
+        public int Id { get; set; }
+
+        #endregion
+    }
+}
