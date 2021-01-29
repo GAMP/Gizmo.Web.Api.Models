6fe3340ef97a0f3b7e6094c07bd6c7147dcdf703
dimitrios <dimitrios@local.local>
2021-01-29 14:47:44 +0200

    Generate IUrlQueryParameters include subobjects of type IUrlQueryParameters.

--- Infrastructure/ParameterGenerator.cs
+++ Infrastructure/ParameterGenerator.cs
@@ -11,7 +11,7 @@ namespace Gizmo.Web.Api.Models
     public static class ParameterGenerator
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Generate(IUrlQueryParameters queryParameters)
+        public static string Generate(IUrlQueryParameters queryParameters, string prefix = null)
         {
             if (queryParameters == null)
                 return null;
@@ -22,6 +22,9 @@ namespace Gizmo.Web.Api.Models
             //create query parameters collection
             var queryCollection = new Dictionary<string, string>();
 
+            var paramPrefix = string.IsNullOrEmpty(prefix) ? "" : $"{prefix}.";
+
+            var subObjectQueries = new List<string>();
 
             //add properties
             foreach (var property in targetProperties)
@@ -30,30 +33,76 @@ namespace Gizmo.Web.Api.Models
                 var propertyValue = property.GetValue(queryParameters);
 
                 //check if null values should be added
-                if(propertyValue!=null)
+                if (propertyValue != null)
                 {
                     //excludes byte of the ICollection if below
-                    if(propertyValue is Byte[] array)
+                    if (propertyValue is Byte[] array)
                     {
                         //specific use of byte array(image case)
-                        if(property.Name == "Image")
-                            queryCollection.Add(property.Name, Convert.ToBase64String(array, 0, array.Length));
+                        if (property.Name == "Image")
+                            queryCollection.Add($"{paramPrefix}{property.Name}", Convert.ToBase64String(array, 0, array.Length));
                     }
-                    else if(propertyValue is System.Collections.ICollection enumerable)
+                    else if (propertyValue is System.Collections.ICollection enumerable)
                     {
-                        foreach(var s in enumerable)
+                        int index = 0;
+                        foreach (var s in enumerable)
                         {
-                            queryCollection.Add(property.Name, s.ToString());
+                            if (s is IUrlQueryParameters subObject)
+                            {
+                                var subObjectQuery = Generate(subObject, $"{paramPrefix}{property.Name}[{index}]");
+                                if (!string.IsNullOrEmpty(subObjectQuery))
+                                {
+                                    subObjectQuery = subObjectQuery.Substring(1); //Remove question mark.
+                                    subObjectQueries.Add(subObjectQuery);
+                                }
+                            }
+                            else
+                            {
+                                queryCollection.Add($"{paramPrefix}{property.Name}[{index}]", s.ToString());
+                            }
+                            index += 1;
+                        }
+                    }
+                    else if (propertyValue is IUrlQueryParameters subObject)
+                    {
+                        var subObjectQuery = Generate(subObject, $"{paramPrefix}{property.Name}");
+                        if (!string.IsNullOrEmpty(subObjectQuery))
+                        {
+                            subObjectQuery = subObjectQuery.Substring(1); //Remove question mark.
+                            subObjectQueries.Add(subObjectQuery);
                         }
                     }
                     else
                     {
-                        queryCollection.Add(property.Name, propertyValue.ToString());
-                    }                   
-                }                   
+                        queryCollection.Add($"{paramPrefix}{property.Name}", propertyValue.ToString());
+                    }
+                }
             }
 
-             return QueryHelpers.AddQueryString(string.Empty, queryCollection);
+            var result = QueryHelpers.AddQueryString(string.Empty, queryCollection);
+
+            //Add all subobjects to the query.
+            foreach (var subObjectQuery in subObjectQueries)
+            {
+                //If there is no base query then add question mark.
+                if (string.IsNullOrEmpty(result))
+                {
+                    if (!string.IsNullOrEmpty(subObjectQuery))
+                    {
+                        result += $"?{subObjectQuery}";
+                    }
+                }
+                else
+                {
+                    //If there is a base query then add &
+                    if (!string.IsNullOrEmpty(subObjectQuery))
+                    {
+                        result += $"&{subObjectQuery}";
+                    }
+                }
+            }
+
+            return result;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
