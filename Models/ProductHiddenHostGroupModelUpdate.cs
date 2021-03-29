67808360bee6c819925c9a98ed03dea7785c0480
klapsis-netprojects <klapsis@netprojects.gr>
2021-03-29 11:02:57 +0300

    Add ProductHostHidden table.

---
+++ Models/ProductHiddenHostGroupModelUpdate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product hidden host group.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class ProductHiddenHostGroupModelUpdate : ProductHiddenHostGroupModelBase, IEntityBase, IUrlQueryParameters
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the object.
+        /// </summary>
+        [DataMember]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Id of the product.
+        /// </summary>
+        [DataMember]
+        [Required]
+        public int ProductId { get; set; }
+
+        #endregion
+    }
+}
