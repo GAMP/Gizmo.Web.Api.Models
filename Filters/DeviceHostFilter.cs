eddade39046f652e53fb4671af63ce4c93522f2b
Oleg Savelos <olegsavelos@gmail.com>
2021-10-21 15:11:29 +0300

    Devices updates.

---
+++ Filters/DeviceHostFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Filters that can be applied when searching for associated hosts or devices.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class DeviceHostFilter : PaginationFilter, IUrlQueryParameters
+    {
+        #region PROPERTIES
+        
+        /// <summary>
+        /// Specifies explicit object id.
+        /// </summary>
+        [DataMember()]
+        public int? Id
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Specifies host id.
+        /// </summary>
+        [DataMember()]
+        public int? HostId
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Specifies device id.
+        /// </summary>
+        [DataMember()]
+        public int? DeviceId
+        {
+            get; set;
+        } 
+
+        #endregion
+    }
+}
---
+++ Models/DeviceHost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Device host relation model.
+    /// </summary>
+    [Serializable()]
+    [DataContract]
+    public class DeviceHost : IEntityBase
+    {
+        #region PROPERTIES
+        
+        /// <summary>
+        /// Gets object id. 
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets host id.
+        /// </summary>
+        [DataMember]
+        public int HostId
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets device id.
+        /// </summary>
+        [DataMember]
+        public int DeviceId
+        {
+            get; set;
+        } 
+
+        #endregion
+    }
+}
