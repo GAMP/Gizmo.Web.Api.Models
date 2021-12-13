a0c66b37ba1372be8987206127937030a816c61a
Oleg Savelos <olegsavelos@gmail.com>
2021-12-13 14:37:56 +0200

    Updates.

--- Models/VariableModelBase.cs
+++ Models/VariableModelBase.cs
@@ -1,5 +1,4 @@
 ﻿using MessagePack;
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
--- Models/VariableModelCreate.cs
+++ Models/VariableModelCreate.cs
@@ -1,6 +1,4 @@
 ﻿using MessagePack;
-using System;
-using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace Gizmo.Web.Api.Models
