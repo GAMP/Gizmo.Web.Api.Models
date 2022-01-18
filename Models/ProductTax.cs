685ed6b58c81c55d303c00326d40aa520c9e2afe
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2022-01-18 12:51:01 +0200

    Add ProductTax models.

---
+++ Models/ProductTax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product tax.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class ProductTax : ProductTaxModelBase, IEntityBase
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the object.
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Id of the product this tax belongs to.
+        /// </summary>
+        [DataMember]
+        public int ProductId { get; set; }
+
+        #endregion
+    }
+}
