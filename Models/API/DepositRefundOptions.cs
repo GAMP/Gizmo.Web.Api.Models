1dee74af091b27f767272bd4d6810bf4170559a9
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2022-04-01 13:18:18 +0300

    Deposit refund options added.

---
+++ Models/API/DepositRefundOptions.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using System;
+using System.Runtime.Serialization;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Deposit refund options.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    [MessagePackObject]
+    public class DepositRefundOptions : RefundOptions
+    {
+        /// <summary>
+        /// The amount to refund, if null the total amount will be refunded.
+        /// </summary>
+        [DataMember]
+        [MessagePack.Key(100)]
+        public decimal? Amount { get; set; }
+    }
+}
