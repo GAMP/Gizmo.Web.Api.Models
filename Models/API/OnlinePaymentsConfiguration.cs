a86b3fadd4226e9df68e451e90bae3228d1db23a
Oleg Savelos <olegsavelos@gmail.com>
2022-09-24 18:22:16 +0300

    Updates.

--- Models/API/OnlinePaymentsConfiguration.cs
+++ Models/API/OnlinePaymentsConfiguration.cs
@@ -1,15 +1,11 @@
 ﻿using MessagePack;
-using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Online payments configuration model.
     /// </summary>
-    [Serializable]
-    [DataContract]
     [MessagePackObject()]
     public class OnlinePaymentsConfiguration
     {
@@ -18,22 +14,19 @@ namespace Gizmo.Web.Api.Models
         /// <summary>
         /// The preset amounts.
         /// </summary>
-        [DataMember]
-        [MessagePack.Key(0)]
+        [Key(0)]
         public List<decimal> Presets { get; set; }
 
         /// <summary>
         /// Indicates whether the user can set custom value.
         /// </summary>
-        [DataMember]
-        [MessagePack.Key(1)]
+        [Key(1)]
         public bool AllowCustomValue { get; set; }
 
         /// <summary>
         /// The minimum allowed amount.
         /// </summary>
-        [DataMember]
-        [MessagePack.Key(2)]
+        [Key(2)]
         public decimal MinimumAmount { get; set; }
 
         #endregion
