82a9a515d42164fe56d912be158447451a3a9d45
Oleg Savelos <olegsavelos@gmail.com>
2022-09-30 17:14:13 +0300

    Updates.

--- Models/API/Result/AccountCreationByTokenCompleteResult.cs
+++ Models/API/Result/AccountCreationByTokenCompleteResult.cs
@@ -7,15 +7,15 @@ namespace Gizmo.Web.Api.Models
     /// Account creation by token result model.
     /// </summary>
     [MessagePackObject()]
-    public sealed class AccountCreationByTokenCompleteResult : TokenResultWithCodeBase<AccountCreationByTokenCompleteResultCode>
+    public sealed class AccountCreationByTokenCompleteResult : ResultCodeBase<AccountCreationByTokenCompleteResultCode>
     {
         #region PROPERTIES
 
         /// <summary>
         /// Newly created user id.
         /// </summary>
-        [Key(3)]
-        [JsonPropertyOrder(3)]
+        [Key(1)]
+        [JsonPropertyOrder(1)]
         public int? CreatedUserId
         {
             get; set;
