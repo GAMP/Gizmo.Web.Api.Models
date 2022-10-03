1d1a6f0c2266b200bc202ee3fcc62d8453162a79
Oleg Savelos <olegsavelos@gmail.com>
2022-10-03 21:33:48 +0300

    Added new metadata models.

---
+++ Models/Messages/Models/EventGroupMetadata.cs
@@ -0,0 +1,37 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Messaging.Models
+{
+    /// <summary>
+    /// Event group metadata.
+    /// </summary>
+    public sealed class EventGroupMetadata
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Event group name.
+        /// </summary>
+        public string Name { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Event group description.
+        /// </summary>
+        public string Description { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Object type.
+        /// </summary>
+        public string Type { get;init;} =string.Empty;
+
+        /// <summary>
+        /// Events.
+        /// </summary>
+        public IEnumerable<EventMetadata> Events { get; init; } = Enumerable.Empty<EventMetadata>();
+
+        #endregion
+    }
+}
---
+++ Models/Messages/Models/EventMetadata.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+namespace Gizmo.Web.Api.Messaging.Models
+{
+    /// <summary>
+    /// Event metadata.
+    /// </summary>
+    public sealed class EventMetadata
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Event name.
+        /// </summary>
+        public string Name { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Event description.
+        /// </summary>
+        public string Description { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Object type.
+        /// </summary>
+        public string Type { get; init; } = string.Empty;
+
+        /// <summary>
+        /// Event id used to identify the event.
+        /// </summary>
+        public int EventId { get; init; } = 0;
+
+        #endregion
+    }
+}
