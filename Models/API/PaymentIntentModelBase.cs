03b0d98b4b137dab6a6085d9695df62d6c396fe9
klapsis-netprojects <klapsis@netprojects.gr>
2022-12-15 13:35:34 +0200

    Add payment intent models.

---
+++ Models/API/PaymentIntentModelBase.cs
@@ -0,0 +1,58 @@
+﻿using MessagePack;
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payment intent.
+    /// </summary>
+    [MessagePackObject]
+    public class PaymentIntentModelBase
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The date that the payment intent was created.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// The Id of the user this payment intent is associated with.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// The Id of the payment method this payment intent is associated with.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int PaymentMethodId { get; set; }
+
+        /// <summary>
+        /// The amount of the payment intent.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// The state of the payment intent.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public PaymentIntentState State { get; set; }
+
+        [MessagePack.Key(5)]
+        public string TransactionId { get; set; }
+
+        [MessagePack.Key(6)]
+        public DateTime? TransactionTime { get; set; }
+
+        /// <summary>
+        /// The GUID of the provider this payment intent is associated with.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public Guid Provider { get; set; }
+
+        #endregion
+    }
+}
---
+++ Models/Filters/PaymentIntentsFilter.cs
@@ -0,0 +1,41 @@
+﻿using MessagePack;
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Filters that can be applied when searching for payment intents.
+    /// </summary>
+    [Serializable]
+    [MessagePackObject]
+    public class PaymentIntentsFilter : PaginationFilter, IUrlQueryParameters
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Return payment intents where the date greater than or equal to the specified date.
+        /// </summary>
+        [Key(200)]
+        public DateTime? DateFrom { get; set; }
+
+        /// <summary>
+        /// Return payment intents where the date less than or equal to the specified date.
+        /// </summary>
+        [Key(201)]
+        public DateTime? DateTo { get; set; }
+
+        /// <summary>
+        /// Return payment intents with the specified state.
+        /// </summary>
+        [Key(202)]
+        public PaymentIntentState? State { get; set; }
+
+        /// <summary>
+        /// Return payment intents of the specified user.
+        /// </summary>
+        [Key(203)]
+        public int? UserId { get; set; }
+
+        #endregion
+    }
+}
