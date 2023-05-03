6ef46be58d4f33a02d443f186102c36c1cda5a77
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-03 14:19:59 +0300

    Update user orders.

---
+++ Models/API/Request/User/Order/UserOrderCreateResultModel.cs
@@ -0,0 +1,36 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User order create result model.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public class UserOrderCreateResultModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The result for the order.
+        /// </summary>
+        [Key(0)]
+        public UserOrderCreateResult Result { get; init; }
+
+        /// <summary>
+        /// The Id of the newly created object if was successfull.
+        /// </summary>
+        [Key(1)]
+        public int? Id { get; init; }
+
+        /// <summary>
+        /// The lines of the order.
+        /// </summary>
+        [Key(2)]
+        public IEnumerable<UserOrderLineCreateResultModel> OrderLines { get; set; } = Enumerable.Empty<UserOrderLineCreateResultModel>();
+
+        #endregion
+    }
+}
