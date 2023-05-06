f963af07b5fcf5384d86a3c38c1edc482cf016eb
Oleg Savelos <olegsavelos@gmail.com>
2023-05-06 09:29:50 +0300

    Experiment.

--- Models/API/Request/PaginationCursor.cs
+++ Models/API/Request/PaginationCursor.cs
@@ -5,6 +5,7 @@ using MessagePack;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
 
 namespace Gizmo.Web.Api.Models
 {
@@ -53,9 +54,42 @@ namespace Gizmo.Web.Api.Models
     public sealed class CursorBase64Converter : JsonConverter<PaginationCursor>
     {
         /// <inheritdoc/>
-        public override PaginationCursor Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-            throw new NotSupportedException();
+        public override PaginationCursor Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            //get decoded json bytes
+            var bytes = reader.GetBytesFromBase64();
 
+            //create new json reader
+            var jsonReader = new Utf8JsonReader(bytes);
+
+            //parse the cursor
+            var pcursor = new PaginationCursor();
+
+            while (jsonReader.Read())
+            {
+                if (jsonReader.TokenType == JsonTokenType.PropertyName)
+                {
+                    var propertyName = jsonReader.GetString()?.ToLower();
+                    jsonReader.Read();
+                    switch (propertyName)
+                    {
+                        case "id":
+                            pcursor.Id = jsonReader.GetInt32();
+                            break;
+                        case "name":
+                            pcursor.Name = jsonReader.GetString()!;
+                            break;
+                        case "value":
+                            pcursor.Value = jsonReader.GetString()!;
+                            break;
+                        case "isforward":
+                            pcursor.IsForward = jsonReader.GetBoolean();
+                            break;
+                    }
+                }
+            }
+            return pcursor;
+        }
         /// <inheritdoc/>
         public override void Write(Utf8JsonWriter writer, PaginationCursor value, JsonSerializerOptions options) =>
             writer.WriteBase64StringValue(JsonSerializer.SerializeToUtf8Bytes(value, options));
@@ -66,6 +100,7 @@ namespace Gizmo.Web.Api.Models
     /// </summary>
     public sealed class CursorModelBinder : IModelBinder
     {
+        private static readonly CursorBase64Converter Converter = new();
         /// <inheritdoc/>
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
@@ -73,37 +108,10 @@ namespace Gizmo.Web.Api.Models
             if (!bindingContext.HttpContext.Request.Query.TryGetValue("Pagination.Cursor", out var cursor))
                 return Task.CompletedTask;
 
-            //decode base64 string to a byte array
-            var jsonArray = Convert.FromBase64String(cursor);
+            var e = new Utf8JsonReader(System.Text.Encoding.UTF8.GetBytes(cursor));
+            var model = Converter.Read(ref e, typeof(PaginationCursor), new JsonSerializerOptions());
 
-            var reader2 = new Utf8JsonReader(jsonArray);
-
-            var pcursor = new PaginationCursor();
-            while (reader2.Read())
-            {
-                if (reader2.TokenType == JsonTokenType.PropertyName)
-                {
-                    var propertyName = reader2.GetString()?.ToLower();
-                    reader2.Read();
-                    switch (propertyName)
-                    {
-                        case "id":
-                            pcursor.Id = reader2.GetInt32();
-                            break;
-                        case "name":
-                            pcursor.Name = reader2.GetString()!;
-                            break;
-                        case "value":
-                            pcursor.Value = reader2.GetString()!;
-                            break;
-                        case "isforward":
-                            pcursor.IsForward = reader2.GetBoolean();
-                            break;
-                    }
-                }
-            }
-
-            bindingContext.Result = ModelBindingResult.Success(pcursor);
+            bindingContext.Result = ModelBindingResult.Success(model);
 
             return Task.CompletedTask;
         }
