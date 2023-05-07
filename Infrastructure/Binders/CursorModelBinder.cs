ec3032a72d9370869a1a29d46d60d5309e602997
Oleg Savelos <olegsavelos@gmail.com>
2023-05-07 18:13:38 +0300

    Base64 cursor.

---
+++ Infrastructure/Binders/CursorModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using Gizmo.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Gizmo.Web.Api
+{
+    /// <summary>
+    /// Binds <see cref="PaginationCursor"/> class from a base64 encoded json string.
+    /// </summary>
+    public sealed class CursorModelBinder : IModelBinder
+    {
+        private static readonly Type SupportedType = typeof(ModelFilterPagination);
+
+        /// <inheritdoc/>
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            //see if cursor provided as query parameter
+            //the parameter should be provided when we want to use cursor, if not provided the cursor model will be set 
+            //to its default value which is null on the target object
+            if (!bindingContext.HttpContext.Request.Query.TryGetValue("Pagination.Cursor", out var cursor) || !cursor.Any())
+                return Task.CompletedTask;
+
+            //check if the type of the model is supported
+            Type? containerType = bindingContext.ModelMetadata.ContainerType;
+            if (containerType == null || containerType != SupportedType)
+                return Task.CompletedTask;
+
+            try
+            {
+                var span = Convert.FromBase64String(cursor);
+                var reader = new Utf8JsonReader(span);
+                var model = CursorBase64Converter.Read(ref reader);
+                bindingContext.Result = ModelBindingResult.Success(model);
+            }
+            catch
+            {
+                //possibly log
+
+                //we have failed to bind, TODO figure out how this affects the request pipeline
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Models/API/Request/PaginationCursor.cs
+++ Models/API/Request/PaginationCursor.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Text.Json.Serialization;
-using System.Text.Json;
 using MessagePack;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Nodes;
 
 namespace Gizmo.Web.Api.Models
 {
@@ -46,74 +41,5 @@ namespace Gizmo.Web.Api.Models
         public bool IsForward { get; set; } = true;
 
         #endregion
-    }
-
-    /// <summary>
-    /// Converts pagination cursor to base64 encoded json string.
-    /// </summary>
-    public sealed class CursorBase64Converter : JsonConverter<PaginationCursor>
-    {
-        /// <inheritdoc/>
-        public override PaginationCursor Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            //get decoded json bytes
-            var bytes = reader.GetBytesFromBase64();
-
-            //create new json reader
-            var jsonReader = new Utf8JsonReader(bytes);
-
-            //parse the cursor
-            var pcursor = new PaginationCursor();
-
-            while (jsonReader.Read())
-            {
-                if (jsonReader.TokenType == JsonTokenType.PropertyName)
-                {
-                    var propertyName = jsonReader.GetString()?.ToLower();
-                    jsonReader.Read();
-                    switch (propertyName)
-                    {
-                        case "id":
-                            pcursor.Id = jsonReader.GetInt32();
-                            break;
-                        case "name":
-                            pcursor.Name = jsonReader.GetString()!;
-                            break;
-                        case "value":
-                            pcursor.Value = jsonReader.GetString()!;
-                            break;
-                        case "isforward":
-                            pcursor.IsForward = jsonReader.GetBoolean();
-                            break;
-                    }
-                }
-            }
-            return pcursor;
-        }
-        /// <inheritdoc/>
-        public override void Write(Utf8JsonWriter writer, PaginationCursor value, JsonSerializerOptions options) =>
-            writer.WriteBase64StringValue(JsonSerializer.SerializeToUtf8Bytes(value, options));
-    }
-
-    /// <summary>
-    /// Binds <see cref="PaginationCursor"/> class from a base64 encoded json string.
-    /// </summary>
-    public sealed class CursorModelBinder : IModelBinder
-    {
-        private static readonly CursorBase64Converter Converter = new();
-        /// <inheritdoc/>
-        public Task BindModelAsync(ModelBindingContext bindingContext)
-        {
-            //see if cursor provided as query parameter
-            if (!bindingContext.HttpContext.Request.Query.TryGetValue("Pagination.Cursor", out var cursor))
-                return Task.CompletedTask;
-
-            var e = new Utf8JsonReader(System.Text.Encoding.UTF8.GetBytes(cursor));
-            var model = Converter.Read(ref e, typeof(PaginationCursor), new JsonSerializerOptions());
-
-            bindingContext.Result = ModelBindingResult.Success(model);
-
-            return Task.CompletedTask;
-        }
     }
 }
