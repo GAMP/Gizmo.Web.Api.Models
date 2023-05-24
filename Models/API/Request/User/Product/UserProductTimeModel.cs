c6dc9140f7a974d3ed3b039da15cc61457dde7ef
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-24 09:16:20 +0300

    Add expiration options.

--- Models/API/Request/User/Product/UserProductTimeModel.cs
+++ Models/API/Request/User/Product/UserProductTimeModel.cs
@@ -30,9 +30,39 @@ namespace Gizmo.Web.Api.Models
         public IEnumerable<int> DisallowedHostGroups { get; set; } = Enumerable.Empty<int>();
 
         /// <summary>
-        /// Whether the product is restricted for current host group.
+        /// Gets or sets expire after days.
         /// </summary>
         [Key(3)]
+        public int ExpiresAfter { get; set; }
+
+        /// <summary>
+        /// Gets or sets expiration options.
+        /// </summary>
+        [Key(4)]
+        public ProductTimeExpirationOptionType ExpirationOptions { get; set; }
+
+        /// <summary>
+        /// Gets or sets expire from options.
+        /// </summary>
+        [Key(5)]
+        public ExpireFromOptionType ExpireFromOptions { get; set; }
+
+        /// <summary>
+        /// Gets or sets expire after type.
+        /// </summary>
+        [Key(6)]
+        public ExpireAfterType ExpireAfterType { get; set; }
+
+        /// <summary>
+        /// Gets or sets expire at day time minute.
+        /// </summary>
+        [Key(7)]
+        public int ExpireAtDayTimeMinute { get; set; }
+
+        /// <summary>
+        /// Whether the product is restricted for current host group.
+        /// </summary>
+        [Key(8)]
         public bool IsRestrictedForHostGroup { get; set; }
     }
 }
