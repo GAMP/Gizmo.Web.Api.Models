6cc65beec14d2edd05fb7357a530e6da6b05e9b6
Andrey Pestunov <masterlifting@gmail.com>
2023-08-28 12:46:27 +0200

    fix: delete the constructors from POCO

--- Models/API/Request/User/Usage/UserUsageTimeLevelModel.cs
+++ Models/API/Request/User/Usage/UserUsageTimeLevelModel.cs
@@ -10,54 +10,6 @@ namespace Gizmo.Web.Api.Models
     [Serializable, MessagePackObject]
     public sealed class UserUsageTimeLevelModel : IUserUsageTimeLevelModel
     {
-        #region CONSTRUCTOR
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public UserUsageTimeLevelModel()
-        {
-            UsageType = UsageType.None;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        public UserUsageTimeLevelModel(UserUsageRateModel model)
-        {
-            Rate = model;
-            UsageType = UsageType.Rate;
-            
-            AvailableMinutes = model.AvailableMinutes;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        public UserUsageTimeLevelModel(UserUsageTimeOfferModel model)
-        {
-            TimeOffer = model;
-            UsageType = UsageType.TimeOffer;
-            
-            AvailableMinutes = model.AvailableMinutes;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        public UserUsageTimeLevelModel(UserUsageTimeFixedModel model)
-        {
-            TimeFixed = model;
-            UsageType = UsageType.TimeFixed;
-            
-            AvailableMinutes = model.AvailableMinutes;
-        }
-
-        #endregion
-
         /// <inheritdoc/>
         [Key(0)] public UserUsageRateModel? Rate { get; init; }
 
