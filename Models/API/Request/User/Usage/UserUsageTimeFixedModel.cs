ba034f2ba4fa7594f5ae4d752a0417b1ed0f3a54
Andrey Pestunov <masterlifting@gmail.com>
2023-09-05 14:34:36 +0200

    chore: add models descriptions

--- Models/API/Request/User/Usage/UserUsageTimeFixedModel.cs
+++ Models/API/Request/User/Usage/UserUsageTimeFixedModel.cs
@@ -5,28 +5,28 @@ using MessagePack;
 namespace Gizmo.Web.Api.Models
 {
     /// <summary>
-    /// 
+    /// TODO: Make the description
     /// </summary>
     [Serializable, MessagePackObject]
     public sealed class UserUsageTimeFixedModel
     {
         /// <summary>
-        /// 
+        /// Left available minutes of the usage type.
         /// </summary>
         [Key(0)] public int AvailableMinutes { get; set; }
 
         /// <summary>
-        /// 
+        /// Id of the invoice line that was used to purchase the usage type.
         /// </summary>
         [Key(1)] public int InvoiceLineId { get; init; }
 
         /// <summary>
-        /// 
+        /// Purchase date of the usage type.
         /// </summary>
         [Key(2)] public DateTime PurchaseDate { get; init; }
 
         /// <summary>
-        /// 
+        /// Total minutes of the usage type.
         /// </summary>
         [Key(3)] public int TotalMinutes { get; init; }
     }
