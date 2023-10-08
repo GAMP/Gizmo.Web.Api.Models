dece3d3d90b46e3ce74a4af9c4392072d84ffce1
Andrey Pestunov <masterlifting@gmail.com>
2023-10-08 20:06:53 +0200

    refactor: delete  unusable props

--- Models/API/Request/User/Usage/UserUsageTimeOfferModel.cs
+++ Models/API/Request/User/Usage/UserUsageTimeOfferModel.cs
@@ -16,38 +16,28 @@ namespace Gizmo.Web.Api.Models
         [Key(0)] public int AvailableMinutes { get; set; }
 
         /// <summary>
-        /// Start time of availability the usage type
-        /// </summary>
-        [Key(1)] public DateTime AvailableTime { get; set; }
-
-        /// <summary>
         /// Expiration time of the usage type. Null if the usage type does not expire.
         /// </summary>
-        [Key(2)] public DateTime? ExpirationTime { get; set; }
+        [Key(1)] public DateTime? ExpirationTime { get; set; }
 
         /// <summary>
         /// Id of the InvoiceLine that was used to purchase the usage type.
         /// </summary>
-        [Key(3)] public int InvoiceLineId { get; init; }
+        [Key(2)] public int InvoiceLineId { get; init; }
 
         /// <summary>
         /// Id of the product that was used to purchase the usage type.
         /// </summary>
-        [Key(4)] public int ProductId { get; init; }
+        [Key(3)] public int ProductId { get; init; }
 
         /// <summary>
         /// Purchase date of the product.
         /// </summary>
-        [Key(5)] public DateTime PurchaseTime { get; set; }
+        [Key(4)] public DateTime PurchaseTime { get; set; }
 
         /// <summary>
         /// First usage time of the usage type.
         /// </summary>
-        [Key(6)] public DateTime? FirstUsageTime { get; set; }
-
-        /// <summary>
-        /// Availability of the host.
-        /// </summary>
-        [Key(7)] public bool IsAvailable { get; init; }
+        [Key(5)] public DateTime? FirstUsageTime { get; set; }
     }
 }
