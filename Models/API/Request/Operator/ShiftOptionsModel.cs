1a6db2201caf26345d141a4410e89622be01427f
Oleg Savelos <olegsavelos@gmail.com>
2023-10-12 17:22:28 +0300

    added shift options model.

---
+++ Models/API/Request/Operator/ShiftOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Shift options model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class ShiftOptionsModel : IWebApiModel
+    {
+        /// <summary>
+        /// Gets shift options.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ShiftOptions Options
+        {
+            get; init;
+        }
+    }
+}
