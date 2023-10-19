0909abde2c670ee48ce98b9972079c09b02c76e3
Oleg Savelos <olegsavelos@gmail.com>
2023-10-19 15:42:38 +0300

    password update model.

---
+++ Abstractions/Models/API/Request/IOperatorPasswordUpdateModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Operator password update mode.
+    /// </summary>
+    public interface IOperatorPasswordUpdateModel : IWebApiModel 
+    {
+        /// <summary>
+        /// New password.
+        /// </summary>
+        public string Password { get; init; }        
+    }
+}
---
+++ Models/API/Request/Operator/Password/OperatorPasswordUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models.Models
+{
+    /// <summary>
+    /// <inheritdoc cref="IOperatorPasswordUpdateModel"/>
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class OperatorPasswordUpdateModel : IOperatorPasswordUpdateModel
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(0)]
+        [Required()]
+        public string Password
+        {
+            get;init;
+        } = string.Empty;
+    }
+}
