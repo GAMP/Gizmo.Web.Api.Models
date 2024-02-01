2a42222290401a31917934d6014322d378ff0c36
Oleg Savelos <olegsavelos@gmail.com>
2024-02-01 18:39:15 +0200

    reports updates.

---
+++ Services/IReportService.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Services
+{
+    /// <summary>
+    /// Web report generation servcice.
+    /// </summary>
+    /// <remarks>
+    /// This service will have two implementations.<br></br><br></br>
+    /// 1) Gizmo server side, the service will call the database directly to generate a report.<br></br>
+    /// 2) Gizmo web manager side, the service will use web api to obtain reports, esentially web api endpoints will be calling into same server side implementation.<br></br><br></br>
+    /// <b>The service interface is located in web api models library due to exposure to web report models.</b>
+    /// </remarks>
+    public interface IReportService
+    {
+    }
+}
