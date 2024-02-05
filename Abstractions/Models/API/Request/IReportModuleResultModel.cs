b5b0d2a3a011b9915ac03e944040b4def0e9082b
Oleg Savelos <olegsavelos@gmail.com>
2024-02-05 14:39:51 +0200

    new models.

---
+++ Abstractions/Models/API/Request/IReportModuleResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gizmo.Web.Api.Models.Abstractions
+{
+    /// <summary>
+    /// Report module result model.
+    /// </summary>
+    public interface IReportModuleResultModel : IWebApiModel
+    {
+    }
+}
---
+++ Models/API/Request/ReportModules/ReportModuleResultModel.cs
@@ -0,0 +1,27 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// <inheritdoc cref="IReportModuleResultModel"/>
+    /// </summary>
+    /// <typeparam name="TReportModel"></typeparam>
+    /// <typeparam name="TAdditionalModel"></typeparam>
+    [MessagePack.MessagePackObject()]
+    public sealed class ReportModuleResultModel<TReportModel, TAdditionalModel> : IReportModuleResultModel
+       where TReportModel : class
+       where TAdditionalModel : class
+    {
+        /// <summary>
+        /// Gets report data.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public TReportModel ReportData { get; init; } = default!;
+
+        /// <summary>
+        /// Gets additional report data.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public TAdditionalModel? AdditionalData { get; init; }
+    }
+}
