a7471d979ecf3087b52b30387cb59b8182b9ea8a
Oleg Savelos <olegsavelos@gmail.com>
2024-02-08 11:25:38 +0200

    updates.

--- Infrastructure/ReportTypeHelper.cs
+++ Infrastructure/ReportTypeHelper.cs
@@ -26,23 +26,27 @@ namespace Gizmo.Web.Api
         /// Creates report result type without optional additional model.
         /// </summary>
         /// <param name="reportType">Report model type.</param>
+        /// <remarks>
+        /// <see cref="EmptyReportAdditionalModel"/> type will be used as additional model.<br></br><br></br>
+        /// <b>Using non <see cref="EmptyReportAdditionalModel"/> as additional model type in report module will cause serialization errors.</b>
+        /// </remarks>
         /// <returns>Result type.</returns>
         public static Type GetReportResultType(Type reportType)
         {
             var argsType = typeof(ReportModuleResultModel<,>);
-            var fullType = argsType.MakeGenericType(reportType, typeof(object));
+            var fullType = argsType.MakeGenericType(reportType, typeof(EmptyReportAdditionalModel));
             return fullType;
         }
 
         /// <summary>
         /// Creates report module result.
         /// </summary>
+        /// <param name="reportResultType">Result type.</param>
         /// <param name="model">Report model.</param>
         /// <param name="additionalModel">Additional report model.</param>
         /// <returns>Report module result of type <see cref="ReportModuleResultModel{TReportModel, TReportModelAdditional}"/> </returns>
-        public static IReportModuleResultModel CreateReportResult(object model, object? additionalModel = null)
+        public static IReportModuleResultModel CreateReportResult(Type reportResultType, object model, object? additionalModel = null)
         {
-            Type reportResultType = GetReportResultType(model.GetType(), additionalModel == null ? typeof(object) : additionalModel.GetType());
             return (IReportModuleResultModel)Activator.CreateInstance(reportResultType, model, additionalModel);
         }
 
