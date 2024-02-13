abc6835a74a412c46268dc7e34fc92430ffb4bc3
klapsis-netprojects <klapsis@netprojects.gr>
2024-02-13 12:16:29 +0200

    Updates.

---
+++ Models/API/Request/Reports/OperatorTransactionModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Operator Transaction.
+    /// </summary>
+    [Serializable]
+    [MessagePackObject]
+    public class OperatorTransactionModel
+    {
+        /// <summary>
+        /// Operator name.
+        /// </summary>
+        [Key(0)]
+        public string OperatorName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The creation time of the transaction.
+        /// </summary>
+        [Key(1)]
+        public DateTime TransactionDate { get; set; }
+
+        /// <summary>
+        /// The creation time of the invoice related to the transaction.
+        /// </summary>
+        [Key(2)]
+        public DateTime? InvoiceDate { get; set; }
+
+        /// <summary>
+        /// The transaction title.
+        /// </summary>
+        [Key(3)]
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The username of the customer for whom the transaction was created.
+        /// </summary>
+        [Key(4)]
+        public string CustomerName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The Id of the invoice related to the transaction.
+        /// </summary>
+        [Key(5)]
+        public int? InvoiceId { get; set; }
+
+        /// <summary>
+        /// The value of the transaction.
+        /// </summary>
+        [Key(6)]
+        public decimal? Value { get; set; }
+
+        /// <summary>
+        /// The amount of money or points moved by the transaction.
+        /// </summary>
+        [Key(7)]
+        public decimal? Total { get; set; }
+
+        /// <summary>
+        /// Payment Method Id.
+        /// </summary>
+        [Key(8)]
+        public int PaymentMethodId { get; set; }
+
+        /// <summary>
+        /// Payment Method Name.
+        /// </summary>
+        [Key(9)]
+        public string PaymentMethodName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Register Name.
+        /// </summary>
+        [Key(10)]
+        public string RegisterName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Note.
+        /// </summary>
+        [Key(11)]
+        public string Note { get; set; } = string.Empty;
+    }
+}
--- Models/API/Request/Reports/TransactionsReportModel.cs
+++ Models/API/Request/Reports/TransactionsReportModel.cs
@@ -1,15 +1,148 @@
-﻿using Gizmo.Web.Api.Models.Abstractions;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Gizmo.Web.Api.Models.Abstractions;
+using Gizmo.Web.Api.Models.Enumerations;
 
 namespace Gizmo.Web.Api.Models
 {
     [MessagePack.MessagePackObject()]
     public sealed class TransactionsReportModel
     {
+        /// <summary>
+        /// Report Name.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Filtered Date From.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public DateTime DateFrom { get; set; }
+
+        /// <summary>
+        /// Filtered Date To.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public DateTime DateTo { get; set; }
+
+        /// <summary>
+        /// Company Name.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public string CompanyName { get; set; } = string.Empty;
+
+        ///// <summary>
+        ///// Report Type.
+        ///// </summary>
+        //[MessagePack.Key(4)]
+        //public ReportTypes ReportType { get; set; }
+
+        /// <summary>
+        /// Filtered Operator Id.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public int? OperatorId { get; set; }
+
+        /// <summary>
+        /// Filtered Operator Name.
+        /// </summary>
+        [MessagePack.Key(6)]
+        public string OperatorName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Filtered Register Id.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public int? RegisterId { get; set; }
+
+        /// <summary>
+        /// Filtered Register Name.
+        /// </summary>
+        [MessagePack.Key(8)]
+        public string RegisterName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Filtered User Id.
+        /// </summary>
+        [MessagePack.Key(9)]
+        public int? UserId { get; set; }
+
+        /// <summary>
+        /// Filtered User Name.
+        /// </summary>
+        [MessagePack.Key(10)]
+        public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Filtered Transactions Log Action Type.
+        /// </summary>
+        [MessagePack.Key(11)]
+        public TransactionsLogActionTypes? TransactionsLogActionType { get; set; }
+
+        /// <summary>
+        /// List of operator transactions.
+        /// </summary>
+        [MessagePack.Key(12)]
+        public List<OperatorTransactionModel> Transactions { get; set; } = new List<OperatorTransactionModel>();
     }
 
     [MessagePack.MessagePackObject()]
-    public sealed class TransactionReportParametersModel : IWebApiModel , IUriParametersQuery
+    public sealed class TransactionReportParametersModel : IWebApiModel, IUriParametersQuery
     {
+        #region PROPERTIES
 
+        /// <summary>
+        /// Filter Date From.
+        /// </summary>
+        [MessagePack.Key(0)]
+        [Required]
+        public DateTime DateFrom { get; set; }
+
+        /// <summary>
+        /// Filter Date To.
+        /// </summary>
+        [MessagePack.Key(1)]
+        [Required]
+        public DateTime DateTo { get; set; }
+
+        /// <summary>
+        /// Filter Operator Id.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int? OperatorId { get; set; }
+
+        /// <summary>
+        /// Filter Register Id.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int? RegisterId { get; set; }
+
+        /// <summary>
+        /// Filter User Id.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int? UserId { get; set; }
+
+        /// <summary>
+        /// Filter User Name.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// Filter Void Operator Id.
+        /// </summary>
+        [MessagePack.Key(6)]
+        public int? VoidOperatorId { get; set; }
+
+        /// <summary>
+        /// Filter Transactions Log Action Type.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public TransactionsLogActionTypes? TransactionsLogActionType { get; set; }
+
+        #endregion
     }
 }
