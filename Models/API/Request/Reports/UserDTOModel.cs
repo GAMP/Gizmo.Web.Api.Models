c9609a9b488861f61c282879d108f031690ccb1c
klapsis-netprojects <klapsis@netprojects.gr>
2024-02-27 12:30:43 +0200

    Updates.

---
+++ Models/API/Request/Reports/UserDTOModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User's Information.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public class UserDTOModel
+    {
+        /// <summary>
+        /// User's username.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// User's first name.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// User's last name.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// User's birth date.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public DateTime? BirthDate { get; set; }
+
+        /// <summary>
+        /// Registration date of the user.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public DateTime Registered { get; set; }
+
+        /// <summary>
+        /// The Id of the group to which the user belongs.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public int GroupId { get; set; }
+
+        /// <summary>
+        /// The name of the group to which the user belongs.
+        /// </summary>
+        [MessagePack.Key(6)]
+        public string? GroupName { get; set; }
+
+        /// <summary>
+        /// User's country.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public string? Country { get; set; }
+
+        /// <summary>
+        /// User's city.
+        /// </summary>
+        [MessagePack.Key(8)]
+        public string? City { get; set; }
+
+        /// <summary>
+        /// User's address.
+        /// </summary>
+        [MessagePack.Key(9)]
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// User's e-mail.
+        /// </summary>
+        [MessagePack.Key(10)]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// User's phone number.
+        /// </summary>
+        [MessagePack.Key(11)]
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// User's mobile phone number.
+        /// </summary>
+        [MessagePack.Key(12)]
+        public string? MobilePhone { get; set; }
+
+        /// <summary>
+        /// User's postal code.
+        /// </summary>
+        [MessagePack.Key(13)]
+        public string? PostCode { get; set; }
+
+        /// <summary>
+        /// User Id.
+        /// </summary>
+        [MessagePack.Key(14)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User's gender.
+        /// </summary>
+        [MessagePack.Key(15)]
+        public Sex Sex { get; set; }
+
+        /// <summary>
+        /// User is banned.
+        /// </summary>
+        [MessagePack.Key(16)]
+        public bool IsBanned { get; set; }
+    }
+}
--- Models/API/Request/Reports/UserReportModel.cs
+++ Models/API/Request/Reports/UserReportModel.cs
@@ -1,14 +1,116 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
 using Gizmo.Web.Api.Models.Abstractions;
+using System.Collections.Generic;
 
 namespace Gizmo.Web.Api.Models
 {
+    /// <summary>
+    /// User Report.
+    /// </summary>
     [MessagePack.MessagePackObject()]
     public sealed class UserReportModel : IWebApiModel
     {
+        /// <summary>
+        /// User information.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public UserDTOModel User { get; set; } = new UserDTOModel();
+
+        /// <summary>
+        /// Points earned.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int PointsEarned { get; set; }
+
+        /// <summary>
+        /// Points used.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int Redeemed { get; set; }
+
+        /// <summary>
+        /// Number of logins.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int Logins { get; set; }
+
+        /// <summary>
+        /// Total session time as text.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public string? LoginTime { get; set; }
+
+        /// <summary>
+        /// Total amount of deposits.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public decimal Deposits { get; set; }
+
+        /// <summary>
+        /// Total amount of withdrawals.
+        /// </summary>
+        [MessagePack.Key(6)]
+        public decimal Withdrawals { get; set; }
+
+        /// <summary>
+        /// Total amount of money spent for products.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public decimal Products { get; set; }
+
+        /// <summary>
+        /// Total amount of money spent on fixed time.
+        /// </summary>
+        [MessagePack.Key(8)]
+        public decimal FixedTime { get; set; }
+
+        /// <summary>
+        /// Total amount of money spent on session time.
+        /// </summary>
+        [MessagePack.Key(9)]
+        public decimal Sessions { get; set; }
+
+        /// <summary>
+        /// Total amount of money spent on time products.
+        /// </summary>
+        [MessagePack.Key(10)]
+        public decimal TimeProducts { get; set; }
+
+        /// <summary>
+        /// Total amount of money spent.
+        /// </summary>
+        [MessagePack.Key(11)]
+        public decimal MoneySpend { get; set; }
+
+        /// <summary>
+        /// Number of assets checked out.
+        /// </summary>
+        [MessagePack.Key(12)]
+        public int CheckOutItems { get; set; }
+
+        /// <summary>
+        /// User's profile picture.
+        /// </summary>
+        [MessagePack.Key(13)]
+        public byte[]? Photo { get; set; }
+
+        /// <summary>
+        /// List of records for the average daily session time chart.
+        /// </summary>
+        [MessagePack.Key(14)]
+        public List<ChartRecordDTOModel> SessionTimeChartRecords { get; set; } = new List<ChartRecordDTOModel>();
+
+        /// <summary>
+        /// List of records for the average daily spend chart.
+        /// </summary>
+        [MessagePack.Key(15)]
+        public List<ChartRecordDTOModel> InvoicesChartRecords { get; set; } = new List<ChartRecordDTOModel>();
     }
 
+    /// <summary>
+    /// User Report Filter.
+    /// </summary>
     [MessagePack.MessagePackObject()]
     public sealed class UserReportParametersModel : IWebApiModel , IUriParametersQuery
     {
@@ -25,5 +127,12 @@ namespace Gizmo.Web.Api.Models
         [MessagePack.Key(1)]
         [Required]
         public DateTime DateTo { get; set; }
+
+        /// <summary>
+        /// Filter User Id.
+        /// </summary>
+        [MessagePack.Key(2)]
+        [Required]
+        public int UserId { get; set; }
     }
 }
