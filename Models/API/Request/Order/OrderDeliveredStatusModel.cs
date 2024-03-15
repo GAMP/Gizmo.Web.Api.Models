6fe9185457828e9625697109da976faceca7cfa2
Oleg Savelos <olegsavelos@gmail.com>
2024-03-15 17:09:16 +0200

    updates.

---
+++ Models/API/Request/Order/OrderDeliveredStatusModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Order delivery status.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class OrderDeliveredStatusModel : IWebApiModel
+    {
+        /// <summary>
+        /// Gets if order is marked as delivered.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public bool IsDelivered { get; init; }
+
+        /// <summary>
+        /// Gets deliver time.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public DateTime? DeliverdTime { get; init; }
+    }
+}
