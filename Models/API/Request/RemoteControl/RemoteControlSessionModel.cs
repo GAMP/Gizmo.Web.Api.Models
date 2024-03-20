2af71a46dcbbd83bf03ed70bbf5f65daf46c07bc
Andrei Pestunov <masterlifting@gmail.com>
2024-03-20 08:13:02 +0100

    Add RemoteControlSessionModel

---
+++ Models/API/Request/RemoteControl/RemoteControlSessionModel.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Remote control model.
+    /// </summary>
+    
+    [Serializable, MessagePackObject]
+    public sealed class RemoteControlSessionModel : IWebApiModel
+    {
+        /// <summary>
+        /// The Id of the remote control session.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The Id of the remote control host.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string Password { get; set; } = null!;
+    }
+}
