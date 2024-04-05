2388e133af31b6a167ac6c3267b9f14045bf37e3
Oleg Savelos <olegsavelos@gmail.com>
2024-04-05 12:34:48 +0300

    updates.

---
+++ Models/Filters/HostDeviceFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host device filter.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostDeviceFilter : IModelFilter<DeviceModel>
+    {
+        /// <summary>
+        /// Filter for cursor-based pagination.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <summary>
+        /// Specifies explicit object id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// Include specified objects in the result.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public List<string> Expand { get; set; } = new();
+
+    }
+}
