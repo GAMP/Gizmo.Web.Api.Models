0d256be98d04e841e585f5e77d8fa140616ebd97
Oleg Savelos <olegsavelos@gmail.com>
2024-04-06 15:32:32 +0300

    add parameters.

---
+++ Models/API/Request/Host/Operations/RemoteSessionStartModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Remote session connection parameters.
+    /// </summary>
+    /// <remarks>
+    /// This parameters are sent to client and used to connect to a remote session on specified server.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class RemoteSessionStartModel
+    {
+        /// <summary>
+        /// Gets remote session server URL.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string ServerUrl
+        {
+            get; init;
+        } = string.Empty;
+
+        /// <summary>
+        /// Gets remote session id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string SessionId
+        {
+            get; init;
+        } = string.Empty;
+
+        /// <summary>
+        /// Gets remote session password.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public string Password
+        {
+            get; init;
+        } = string.Empty;
+    }
+}
