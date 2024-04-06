e8e47c420dfd17a4860f2ff4ef9143cf6e2d66c9
Oleg Savelos <olegsavelos@gmail.com>
2024-04-06 10:32:54 +0300

    screen capture parameters.

---
+++ Models/API/Request/Host/Operations/ScreenCaptureParametersModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Screen capture parameters.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class ScreenCaptureParametersModel
+    {
+        /// <summary>
+        /// Image resize width.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Width
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Image resize height.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int Height
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Image quality.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int ImageQuality
+        {
+            get; init;
+        }
+    }
+}
