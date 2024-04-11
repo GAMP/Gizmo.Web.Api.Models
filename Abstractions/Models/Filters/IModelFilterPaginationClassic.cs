d3a899410fee9a939635907b11689e9c4ab75bb1
Andrei Pestunov <masterlifting@gmail.com>
2024-04-11 11:43:01 +0200

    Add pagination classic

---
+++ Abstractions/Models/Filters/IModelFilterPaginationClassic.cs
@@ -0,0 +1,13 @@
+﻿namespace Gizmo.Web.Api.Models.Abstractions
+{
+    /// <summary>
+    /// Filter for classic pagination.
+    /// </summary>
+    public interface IModelFilterPaginationClassic
+    {
+        /// <summary>
+        /// Filter for classic pagination.
+        /// </summary>
+        public ModelFilterPaginationClassic Pagination { get; set; }
+    }
+}
---
+++ Models/Filters/General/ModelFilterPaginationClassic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Pagination model by classic pagination.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class ModelFilterPaginationClassic
+    {
+        #region PROPERTIES
+
+        ///<summary>
+        /// Gets or sets the page number.
+        /// </summary>
+        [Key(0)]
+        public int Page { get; set; }
+
+        ///<summary>
+        /// Gets or sets the page size.
+        /// </summary>
+        [Key(1)]
+        public int PageSize { get; init; }
+
+
+        #endregion
+    }
+}
