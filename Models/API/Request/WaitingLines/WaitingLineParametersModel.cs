718c51f6e4d93ce241167c038198a53b9c54fbc2
Oleg Savelos <olegsavelos@gmail.com>
2024-04-22 16:59:37 +0300

    updates.

---
+++ Models/API/Request/WaitingLines/WaitingLineParametersModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Waiting line parameters model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class WaitingLineParametersModel : IWebApiModel
+    {
+        /// <summary>
+        /// Enables or disables priorities.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public bool EnablePriorities
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Enables or disables next in line.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public bool EnableNextInLine
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Enables or disables remove on timeout.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool TimeoutRemove
+        {
+            get; init;
+        }
+    }
+}
---
+++ Models/Filters/WaitingLinesFilterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Waiting lines filter model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class WaitingLinesFilterModel : IModelFilter<WaitingLineModel>
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = [];
+    }
+}
