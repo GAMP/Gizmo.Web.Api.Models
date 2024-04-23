0a6f4f719889d5ee957be188a1d199a5293d02da
Oleg Savelos <olegsavelos@gmail.com>
2024-04-23 13:27:32 +0300

    updates.

---
+++ Enumerations/WaitingLineMoveType.cs
@@ -0,0 +1,29 @@
+﻿namespace Gizmo.Web.Api.Models.Enumerations
+{
+    /// <summary>
+    /// Waiting line move types.
+    /// </summary>
+    public enum WaitingLineMoveType
+    {
+        /// <summary>
+        /// Custom.
+        /// </summary>
+        Custom = 0,
+        /// <summary>
+        /// Upd.
+        /// </summary>
+        Up = 1,
+        /// <summary>
+        /// Down.
+        /// </summary>
+        Down = 2,
+        /// <summary>
+        /// Top.
+        /// </summary>
+        Top = 3,
+        /// <summary>
+        /// Bottom.
+        /// </summary>
+        Bottom = 4,
+    }
+}
