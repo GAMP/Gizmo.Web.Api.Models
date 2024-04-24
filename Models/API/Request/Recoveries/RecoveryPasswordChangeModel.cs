46719a9c56fa506e22c4ecd5d6155f33654c1fc0
Oleg Savelos <olegsavelos@gmail.com>
2024-04-24 16:34:23 +0300

    recoveries client added.

---
+++ Models/API/Request/Recoveries/RecoveryPasswordChangeModel.cs
@@ -0,0 +1,15 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Password change model.
+    /// </summary>
+    public sealed class RecoveryPasswordChangeModel : IUriParametersQuery
+    {
+        /// <summary>
+        /// New password.
+        /// </summary>
+        public string NewPassword { get; init; } = string.Empty;
+    }
+}
