85169e926f0fc0f547da5f389e5f5d172799d35c
Oleg Savelos <olegsavelos@gmail.com>
2024-05-14 11:17:24 +0300

    updates.

--- Models/API/Request/PagedListClassic.cs
+++ Models/API/Request/PagedListClassic.cs
@@ -10,29 +10,39 @@ namespace Gizmo.Web.Api.Models
     /// Model for the classic paginated data.
     /// </summary>
     /// <typeparam name="T">Data type.</typeparam>
-    /// <param name="data">Data.</param>
-    /// <param name="totalItems">Total number of items.</param>
-    /// <param name="pageSize"> Page size from the filter.</param>
     /// <exception cref="DivideByZeroException">Thrown when page size is zero.</exception>"
     [Serializable, MessagePackObject]
-    public sealed class PagedListClassic<T>(IEnumerable<T> data, int totalItems, int pageSize)
+    public sealed class PagedListClassic<T>
     {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="data">Data.</param>
+        /// <param name="totalItems">Total number of items.</param>
+        /// <param name="pageSize"> Page size from the filter.</param>
+        public PagedListClassic(IEnumerable<T> data, int totalItems, int pageSize)
+        {
+            Data = data;
+            TotalItems = totalItems;
+            TotalPages = pageSize <= 0 ? 0 : totalItems / pageSize + (totalItems % pageSize == 0 ? 0 : 1);
+        }
+
         /// <summary>
         /// The data of the current result set.
         /// </summary>
         [Key(0)]
-        public IEnumerable<T> Data { get; } = data;
+        public IEnumerable<T> Data { get; }
 
         /// <summary>
         /// The total number of items.
         /// </summary>
         [Key(1)]
-        public int TotalItems { get; } = totalItems;
+        public int TotalItems { get; }
 
         /// <summary>
         /// The total number of pages.
         /// </summary>
         [Key(2)]
-        public int TotalPages { get; } = pageSize <=0 ? 0 : totalItems / pageSize + (totalItems % pageSize == 0 ? 0 : 1);
+        public int TotalPages { get; }
     }
 }
