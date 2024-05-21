38ef1ed2a3675ea1c48ed0ab61788fba4f28b9ca
Oleg Savelos <olegsavelos@gmail.com>
2024-05-21 18:08:42 +0300

    add position model.

---
+++ Models/API/Request/Host/Layout/HostLayoutPositionCreateUpdateModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host layout position create/update model.
+    /// </summary>
+    ///<remarks>
+    ///This model is used to set grid based host position within a host layout group.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostLayoutPositionCreateUpdateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Host layout row.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Row
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Host layout column.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int Column
+        {
+            get; init;
+        }
+    }
+}
