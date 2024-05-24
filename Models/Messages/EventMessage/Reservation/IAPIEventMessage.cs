290bbcaa4be7c34032bcf532106183f65527af58
Oleg Savelos <olegsavelos@gmail.com>
2024-05-24 17:41:05 +0300

    updates.

--- Models/Messages/EventMessage/Reservation/IAPIEventMessage.cs
+++ Models/Messages/EventMessage/Reservation/IAPIEventMessage.cs
@@ -5,6 +5,9 @@ namespace Gizmo.Web.Api.Messaging
     [Union(600, typeof(ReservationCreatedEventMessage))]
     [Union(601, typeof(ReservationUpdatedEventMessage))]
     [Union(602, typeof(ReservationCancelledEventMessage))]
+    [Union(603, typeof(ReservationHostAddedEventMessage))]
+    [Union(604, typeof(ReservationHostReservationUpdatedEventMessage))]
+    [Union(605, typeof(ReservationHostRemovedEventMessage))]
     public partial interface IAPIEventMessage
     {
     }
