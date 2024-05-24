20296ff5d0acc4606d5e70054357fb7776579acb
Oleg Savelos <olegsavelos@gmail.com>
2024-05-24 10:47:44 +0300

    next reservations.

---
+++ Models/API/Request/Host/Reservation/HostNextReservationModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host next reservation model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostNextReservationModel : IWebApiModel
+    {
+        /// <summary>
+        /// Gets host id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int HostId { get; set; }
+
+        /// <summary>
+        /// Gets next reservation id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int? NextReservationId { get; set; } = null!;
+
+        /// <summary>
+        /// Gets next reservation date.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public DateTime? NextReservationTime { get; set; } = null!;
+
+        /// <summary>
+        /// Gets next reservation duration.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int? NextReservationDuration { get; set; } = null!;
+    }
+}
---
+++ Models/Filters/HostNextReservationFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host next reservation filter.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostNextReservationFilter : IModelFilter<HostNextReservationModel>
+    {
+        /// <summary>
+        /// Filter for cursor-based pagination.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <summary>
+        /// Include specified objects in the result.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = new();
+
+        /// <summary>
+        /// Specifies the start date.
+        /// </summary>
+        /// <remarks>
+        /// This parameter will be used as starting point to search for next reservation.<br></br>
+        /// Not specifying this value will cause the search to be done from current date-time.
+        /// </remarks>
+        [MessagePack.Key(2)]
+        public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// Specifies if deleted hosts should be included.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public bool IsDeleted { get; set; }
+    }
+}
