fcc913e0e877409c029e10535cd6af6aed21e5fe
Oleg Savelos <olegsavelos@gmail.com>
2024-05-26 18:52:48 +0300

    user sessions.

---
+++ Models/API/Request/User/UserSession/UserSessionModel.cs
@@ -0,0 +1,63 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+using Gizmo.Web.Api.Models.Enumerations;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User session model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserSessionModel : IWebApiModel, IModelIntIdentifier
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Gets session user id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int UserId
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Gests session host id.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int HostId
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Gets session slot.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int Slot
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Gets session state.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public SessionState State
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Gets session span.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public double Span
+        {
+            get;init;
+        }
+    }
+}
---
+++ Models/Filters/UserSessionFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User session filter.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserSessionFilter : IModelFilter<UserSessionModel>
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public DateTime? DateFrom { get; set; }
+
+        /// <inheritdoc/>
+        [MessagePack.Key(2)]
+        public DateTime? DateTo { get; set; }
+
+        /// <inheritdoc/>
+        [MessagePack. Key(3)]
+        public List<string> Expand { get; set; } = new();
+
+        /// <summary>
+        /// Specific user id filter.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int? UserId
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Specific host id filter.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public int? HostId
+        {
+            get;init;
+        }
+
+        /// <summary>
+        /// Specific session state.
+        /// </summary>
+        [MessagePack.Key(6)]
+        public UserSessionState? State
+        {
+            get;init;
+        }
+    }
+}
