f06c0ec3c7613e8bbceaaff3ff4b2b69896cb422
Oleg Savelos <olegsavelos@gmail.com>
2024-06-05 15:01:08 +0300

    add key.

--- Models/Messages/EventMessage/WaitingLine/IAPIEventMessage.cs
+++ Models/Messages/EventMessage/WaitingLine/IAPIEventMessage.cs
@@ -6,6 +6,7 @@ namespace Gizmo.Web.Api.Messaging
     [Union(401, typeof(WaitingLineUserMoveEventMessage))]
     [Union(402, typeof(WaitingLineUserRemovedEventMessage))]
     [Union(403, typeof(WaitingLineEstimationChangedEventMessage))]
+    [Union(404, typeof(WaitingLineTimedOutEventMessage))]
     public partial interface IAPIEventMessage
     {
     }
