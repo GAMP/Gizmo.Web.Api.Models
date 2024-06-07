267cc6961fdb6f080edb4b84f709c2a0f873df1c
klapsis-netprojects <klapsis@netprojects.gr>
2024-06-07 11:41:34 +0300

    Updates.

---
+++ Models/API/Request/User/Note/UserNoteCountModel.cs
@@ -0,0 +1,41 @@
+﻿using MessagePack;
+
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User note count.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class UserNoteCountModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Number of notes with green severity.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int SeverityGreenCount { get; init; }
+        
+        /// <summary>
+        /// Number of notes with yellow severity.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int SeverityYellowCount { get; init; }
+
+        /// <summary>
+        /// Number of notes with red severity.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int SeverityRedCount { get; init; }
+
+        /// <summary>
+        /// Total number of notes.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int TotalCount { get; init; }
+
+        #endregion
+    }
+}
