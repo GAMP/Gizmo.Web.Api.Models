86b9476ee053d98108cd4733d0a3e478dc47761f
Oleg Savelos <olegsavelos@gmail.com>
2024-06-17 17:00:43 +0300

    order payments model.

---
+++ Models/API/Request/Order/OrderPaymentsCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Order payments parameters.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class OrderPaymentsCreateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Payments to create.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<PaymentCreateModel> Payments { get; init; } = Enumerable.Empty<PaymentCreateModel>();
+    }
+}
---
+++ Models/API/Request/Order/OrderPaymentsCreateResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Order payments create result.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class OrderPaymentsCreateResult : IWebApiModel
+    {
+    }
+}
