db66db43e2fec6140ba9ea75f07c24c0f5fa40f4
klapsis-netprojects <klapsis@netprojects.gr>
2024-06-27 07:52:05 +0300

    Updates.

---
+++ Abstractions/Models/API/Request/ISecurityProfileModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models.Abstractions
+{
+    /// <summary>
+    /// Security profile.
+    /// </summary>
+    public interface ISecurityProfileModel : IWebApiModel
+    {
+        /// <summary>
+        /// The name of the security profile.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// The disabled drives of the security profile.
+        /// </summary>
+        int DisabledDrives { get; set; }
+    }
+}
---
+++ Models/API/Request/SecurityProfile/SecurityProfileModel.cs
@@ -0,0 +1,39 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+using MessagePack;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Security profile.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class SecurityProfileModel : ISecurityProfileModel, IModelIntIdentifier
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the object.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// The name of the security profile.
+        /// </summary>
+        [MessagePack.Key(1)]
+        [StringLength(45)]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// The disabled drives of the security profile.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int DisabledDrives { get; set; }
+
+        #endregion
+    }
+}
