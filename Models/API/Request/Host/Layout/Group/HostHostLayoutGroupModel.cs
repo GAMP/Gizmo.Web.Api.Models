c6a4721beb48836b0573f3bec49eb39690a85bb7
klapsis-netprojects <klapsis@netprojects.gr>
2024-06-27 11:37:58 +0300

    Updates.

---
+++ Models/API/Request/Host/Layout/Group/HostHostLayoutGroupModel.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host host layout group.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class HostHostLayoutGroupModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the host layout group.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int HostLayoutGroupId { get; init; }
+
+        #endregion
+    }
+}
