e6d50b0a7a0859a9167ebc2160893d227cb21328
klapsis-netprojects <klapsis@netprojects.gr>
2024-06-27 07:06:55 +0300

    Updates.

---
+++ Models/API/Request/Skin/SkinModel.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Skin.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class SkinModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The name of the skin.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string Name { get; set; } = null!;
+
+        #endregion
+    }
+}
