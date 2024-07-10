7e5891f83ca1291fe3ce57f7d971b9f8d28cff7b
klapsis-netprojects <klapsis@netprojects.gr>
2024-07-10 10:52:03 +0300

    Updates.

---
+++ Models/API/Request/User/UserLoginResultModel.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User login result.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class UserLoginResultModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Login result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public LoginResult LoginResult { get; init; }
+
+        #endregion
+    }
+}
---
+++ Models/API/Request/User/UserLogoutResultModel.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+
+using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User logout result.
+    /// </summary>
+    [Serializable, MessagePackObject]
+    public sealed class UserLogoutResultModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Logout result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public LogoutResult LogoutResult { get; init; }
+
+        #endregion
+    }
+}
