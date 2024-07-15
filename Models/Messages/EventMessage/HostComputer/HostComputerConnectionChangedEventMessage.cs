418c12f925713d59dc50121730e19d394a8ecc92
Oleg Savelos <olegsavelos@gmail.com>
2024-07-15 17:36:54 +0300

    host connection event message.

---
+++ Models/Messages/EventMessage/HostComputer/HostComputerConnectionChangedEventMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    /// <summary>
+    /// Host computer input lock changed event.
+    /// </summary>
+    [System.ComponentModel.DataAnnotations.Name("Connection changed", "HOST_COMPUTER_CONNECTION_CHANGED_EVENT_NAME")]
+    [System.ComponentModel.DataAnnotations.ExtendedDescription("Connection changed event", "HOST_COMPUTER_CONNECTION_CHANGED_EVENT_DESCRIPTION")]
+    [MessagePack.MessagePackObject()]
+    public sealed class HostComputerConnectionChangedEventMessage : HostComputerEventMessageBase
+    {
+        /// <summary>
+        /// Indicates that host computer is currently connected.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool IsConnected { get;init;}
+    }
+}
--- Models/Messages/EventMessage/HostComputer/IAPIEventMessage.cs
+++ Models/Messages/EventMessage/HostComputer/IAPIEventMessage.cs
@@ -3,6 +3,7 @@
     [MessagePack.Union(1700, typeof(HostComputerMaintenanceChanged))]
     [MessagePack.Union(1701, typeof(HostComputerSecurityChanged))]
     [MessagePack.Union(1702, typeof(HostComputerInputLockChanged))]
+    [MessagePack.Union(1703, typeof(HostComputerConnectionChangedEventMessage))]
     public partial interface IAPIEventMessage
     {
     }
