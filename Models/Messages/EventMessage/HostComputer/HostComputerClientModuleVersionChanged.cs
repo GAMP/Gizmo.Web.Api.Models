0c9859df13822fcfb687bc4ddf3483dd879c5114
Oleg Savelos <olegsavelos@gmail.com>
2024-07-15 18:19:52 +0300

    module version event.

---
+++ Models/Messages/EventMessage/HostComputer/HostComputerClientModuleVersionChanged.cs
@@ -0,0 +1,20 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    /// <summary>
+    /// Client version change message.
+    /// </summary>
+    [System.ComponentModel.DataAnnotations.Name("Client module version changed", "HOST_COMPUTER_CLIENT_MODULE_VERSION_CHANGED_EVENT_NAME")]
+    [System.ComponentModel.DataAnnotations.ExtendedDescription("Client module version changed event", "HOST_COMPUTER_CLIENT_MODULE_VERSION_CHANGED_EVENT_DESCRIPTION")]
+    [MessagePack.MessagePackObject()]
+    public sealed class HostComputerClientModuleVersionChanged : HostComputerEventMessageBase
+    {
+        /// <summary>
+        /// Current client module version.
+        /// </summary>
+        /// <remarks>
+        /// Null if client version is unknown.
+        /// </remarks>
+        [MessagePack.Key(2)]
+        public string? ModuleVersion { get; init; }
+    }
+}
