66ca055ea56210b208fee2f0a3617b6ba96c7141
Oleg Savelos <olegsavelos@gmail.com>
2024-07-16 16:41:37 +0300

    add log cleared event message.

---
+++ Models/Messages/EventMessage/Base/LogEventMessageBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    /// <summary>
+    /// Assistance request event message base.
+    /// </summary>
+    [System.ComponentModel.DataAnnotations.Name("Log", "LOG_EVENT_GROUP_NAME")]
+    [System.ComponentModel.DataAnnotations.ExtendedDescription("Log related events", "LOG_EVENT_GROUP_DESCRIPTION")]
+    [EventGroup(20)]
+    public abstract class LogEventMessageBase : APIEventMessage
+    {
+    }
+}
