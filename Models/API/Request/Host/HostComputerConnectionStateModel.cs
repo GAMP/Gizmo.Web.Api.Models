9bdca4550c998d6a7266e62e6f212e0f6095de31
Oleg Savelos <olegsavelos@gmail.com>
2024-07-16 12:32:01 +0300

    connection states.

---
+++ Models/API/Request/Host/HostComputerConnectionStateModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host connection model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostComputerConnectionStateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Host computer id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int HostComputerId { get; init; }
+
+        /// <summary>
+        /// Indicates that host is connected.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public bool IsConnected { get;init;}
+
+        /// <summary>
+        /// Connection IP Address.
+        /// </summary>
+        /// <remarks>Null in case of disconnected host.</remarks>
+        [MessagePack.Key(2)]
+        public string? IPAddress { get;init;}
+    }
+}
---
+++ Models/Filters/HostComputerConnectionStateFilter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host connection state filter model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class HostComputerConnectionStateFilter : IModelFilter<HostComputerConnectionStateModel>, IBranchBasedModelFilter
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = [];
+
+        /// <inheritdoc/>
+        [MessagePack.Key(2)]
+        public int? BranchId { get; set; }
+
+        /// <summary>
+        /// Host computer id.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int? HostComputerId { get; set; }
+    }
+}
