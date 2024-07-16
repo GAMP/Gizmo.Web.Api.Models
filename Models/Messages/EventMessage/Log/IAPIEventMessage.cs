bcd713d6a7789c54cdcc3b9ad5b0cfc807f3602e
Oleg Savelos <olegsavelos@gmail.com>
2024-07-16 16:50:17 +0300

    ignores updates.

---
+++ Models/Messages/EventMessage/Log/IAPIEventMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    [MessagePack.Union(2001,typeof(LogClearedEventMessage))]
+    public partial interface IAPIEventMessage
+    {
+    }
+}
---
+++ Models/Messages/EventMessage/Log/LogClearedEventMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    /// <summary>
+    /// Log cleared event message.
+    /// </summary>
+    [System.ComponentModel.DataAnnotations.Name("Cleared", "LOG_CLEARED_EVENT_NAME")]
+    [System.ComponentModel.DataAnnotations.ExtendedDescription("Indicates log being cleared", "LOG_CLEARED_EVENT_DESCRIPTION")]
+    [MessagePack.MessagePackObject()]
+    public sealed class LogClearedEventMessage : LogEventMessageBase
+    {
+    }
+}
