124157107bd8cc3e225fffa82e2e4abf819517be
klapsis-netprojects <klapsis@netprojects.gr>
2024-07-26 09:56:13 +0300

    Updates.

---
+++ Models/API/Request/Host/ApplicationDeleteOptionsModel.cs
@@ -0,0 +1,13 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application delete options.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationDeleteOptionsModel : IUriParametersQuery
+    {
+    }
+}
---
+++ Models/API/Request/Host/ApplicationDeleteResultModel.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application delete result.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationDeleteResultModel : IWebApiModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Application delete result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ApplicationDeleteResult ApplicationDeleteResult { get; init; }
+
+        #endregion
+    }
+}
