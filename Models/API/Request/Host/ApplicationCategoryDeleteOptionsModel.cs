754596480f842818e39138da10afd543d1d7546b
klapsis-netprojects <klapsis@netprojects.gr>
2024-07-26 10:37:33 +0300

    Updates.

---
+++ Models/API/Request/Host/ApplicationCategoryDeleteOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application category delete options.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationCategoryDeleteOptionsModel : IUriParametersQuery
+    {
+        /// <summary>
+        /// The id of the application category to move applications before delete.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? MoveToApplicationCategoryId { get; init; }
+    }
+}
---
+++ Models/API/Request/Host/ApplicationCategoryDeleteResultModel.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application category delete result.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationCategoryDeleteResultModel : IWebApiModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Application category delete result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ApplicationCategoryDeleteResult ApplicationCategoryDeleteResult { get; init; }
+
+        #endregion
+    }
+}
