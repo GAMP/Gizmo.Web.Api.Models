8faf414de98ba1d6dfd4738e49942ad53fb42306
klapsis-netprojects <klapsis@netprojects.gr>
2024-09-13 17:05:18 +0300

    Updates.

---
+++ Models/API/Request/Application/ApplicationModelDuplicate.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application duplicate.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationModelDuplicate
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The title of the new executable.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string NewTitle { get; set; } = null!;
+
+        #endregion
+    }
+}
---
+++ Models/API/Request/Application/Executable/ApplicationExecutableModelDuplicate.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Application executable duplicate.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ApplicationExecutableModelDuplicate
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The caption of the new executable.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string NewCaption { get; set; } = null!;
+
+        #endregion
+    }
+}
