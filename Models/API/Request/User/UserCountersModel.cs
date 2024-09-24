63cc6fdacc36f11ba1092769c367ccdf13b4e880
klapsis-netprojects <klapsis@netprojects.gr>
2024-09-24 11:55:14 +0300

    Updates.

---
+++ Models/API/Request/User/UserCountersModel.cs
@@ -0,0 +1,54 @@
+﻿using MessagePack;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User counters.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class UserCountersModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Counters per user group.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<UserGroupCountersModel> CountersPerGroup { get; set; } = Enumerable.Empty<UserGroupCountersModel>();
+
+        /// <summary>
+        /// Total users.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int TotalUsers { get; set; }
+
+        /// <summary>
+        /// Guest users.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int GuestUsers { get; set; }
+
+        /// <summary>
+        /// Banned users.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int BannedUsers { get; set; }
+
+        /// <summary>
+        /// Deleted users.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int DeletedUsers { get; set; }
+
+        /// <summary>
+        /// Total user deposits.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public decimal TotalUserDeposits { get; set; }
+
+        #endregion
+    }
+}
---
+++ Models/API/Request/User/UserGroupCountersModel.cs
@@ -0,0 +1,45 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User group counters.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class UserGroupCountersModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// User group Id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int UserGroupId { get; set; }
+
+        /// <summary>
+        /// Total users within user group.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int TotalUsers { get; set; }
+
+        /// <summary>
+        /// Total guest users within user group.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int GuestUsers { get; set; }
+
+        /// <summary>
+        /// Total banned users within user group.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int BannedUsers { get; set; }
+
+        /// <summary>
+        /// Total deleted users within user group.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int DeletedUsers { get; set; }
+
+        #endregion
+    }
+}
