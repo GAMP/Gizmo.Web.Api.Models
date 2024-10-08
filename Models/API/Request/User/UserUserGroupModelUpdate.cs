a7df627c838f45d0694d967d393b37e92fcc0494
klapsis-netprojects <klapsis@netprojects.gr>
2024-10-08 16:52:06 +0300

    Updates.

---
+++ Models/API/Request/User/UserUserGroupModelUpdate.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User user group.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class UserUserGroupModelUpdate
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the users group id this user belongs to.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int UserGroupId { get; set; }
+
+        #endregion
+    }
+}
