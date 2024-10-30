d8ee04a26cb5badb050abb2693bcd8cf8b9d95d8
klapsis-netprojects <klapsis@netprojects.gr>
2024-10-30 16:41:29 +0200

    Updates.

---
+++ Models/API/Request/User/Group/UserGroupDeleteOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User group delete options.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class UserGroupDeleteOptionsModel : IUriParametersQuery
+    {
+        /// <summary>
+        /// The id of the user group to move users before delete.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? MoveToUserGroupId { get; init; }
+    }
+}
---
+++ Models/API/Request/User/Group/UserGroupDeleteResultModel.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User group delete result.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class UserGroupDeleteResultModel : IWebApiModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// User group delete result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public UserGroupDeleteResult UserGroupDeleteResult { get; init; }
+
+        #endregion
+    }
+}
