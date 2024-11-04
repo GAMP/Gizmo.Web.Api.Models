e0e5d8f46def64e8484ef9c64261d47427069a4a
Oleg Savelos <olegsavelos@gmail.com>
2024-11-04 18:14:52 +0200

    branch related updates.

---
+++ Models/API/Request/Branch/BranchReferenceUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Branch reference update model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class BranchReferenceUpdateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Branch id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int BranchId { get; init; }
+
+        /// <summary>
+        /// Reference state.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public bool IsEnabled { get; init; }
+    }
+}
