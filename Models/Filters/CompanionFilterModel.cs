151de83ed8ea55044acb9a1d9cb34f70934206b3
Oleg Savelos <olegsavelos@gmail.com>
2024-11-06 12:39:43 +0200

    updates.

---
+++ Models/Filters/CompanionFilterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Filter model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CompanionFilterModel : IModelFilter<CompanionModel>
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = new();
+    }
+}
