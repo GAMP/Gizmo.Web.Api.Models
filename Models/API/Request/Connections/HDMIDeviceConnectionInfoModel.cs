9354d5a3eefdd49fd3c381bb48467e010cc6bfb1
Oleg Savelos <olegsavelos@gmail.com>
2024-11-06 15:15:23 +0200

    connection models.

---
+++ Models/API/Request/Connections/HDMIDeviceConnectionInfoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Device connection info model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public class HDMIDeviceConnectionInfoModel : HTTPConnectionInfoModel
+    {
+        /// <summary>
+        /// Device id.
+        /// </summary>
+        [MessagePack.Key(5)]
+        public string? DeviceId
+        {
+            get; init;
+        } = string.Empty;
+    }
+}
---
+++ Models/API/Request/Connections/HTTPConnectionInfoModel.cs
@@ -0,0 +1,56 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Http connection info model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    [MessagePack.Union(0, typeof(CompanionConnectionInfoModel))]
+    [MessagePack.Union(1, typeof(HDMIDeviceConnectionInfoModel))]
+    public abstract class HTTPConnectionInfoModel
+    {
+        /// <summary>
+        /// Connection id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string? ConnectionId
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Remote ip address.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string? RemoteIpAddress
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Local ip address.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public string? LocalIpAddress
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Remote port.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int RemotePort
+        {
+            get; init;
+        }
+
+        /// <summary>
+        /// Local port.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int LocalPort
+        {
+            get; init;
+        }
+    }
+}
