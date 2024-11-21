dd9501280722d4046e5b36470b47cfb03c7bbd48
Oleg Savelos <olegsavelos@gmail.com>
2024-11-21 16:25:33 +0200

    timezone model.

---
+++ Models/API/Request/System/TimeZoneInfoModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Time zone information.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class TimeZoneInfoModel : IWebApiModel
+    {
+        /// <summary>
+        /// Gets or sets the time zone id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public string Id { get; init; } = null!;
+
+        /// <summary>
+        /// Gets or sets the time zone display name.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string DisplayName { get; init; } = null!;
+    }
+}
