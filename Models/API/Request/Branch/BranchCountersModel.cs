7f4ec61416c4e0abe2f51fd9958c917ba4a70c6a
Oleg Savelos <olegsavelos@gmail.com>
2024-12-09 17:42:00 +0200

    add branch counters.

---
+++ Models/API/Request/Branch/BranchCountersModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Branch counters model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class BranchCountersModel : IWebApiModel
+    {
+        /// <summary>
+        /// Branch id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int BranchId { get; init; }
+
+        /// <summary>
+        /// Gets total endpoints.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int Endpoints { get; init; }
+
+        /// <summary>
+        /// Gets total computers.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int Computers { get; init; }
+
+        /// <summary>
+        /// Gets total controllers.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int Controllers { get; init; }
+    }
+}
