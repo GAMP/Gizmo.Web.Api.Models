c6fc0d64955323e5556bb9bc29c02f3f3f736de1
Oleg Savelos <olegsavelos@gmail.com>
2024-12-11 12:58:01 +0200

    add key.

--- Models/Messages/EventMessage/Device/IAPIEventMessage.cs
+++ Models/Messages/EventMessage/Device/IAPIEventMessage.cs
@@ -3,6 +3,7 @@
 namespace Gizmo.Web.Api.Messaging
 {
     [Union(2100, typeof(BarcodeScannedEventMessage))]
+    [Union(2101, typeof(SmartCardReaderDataEventMessage))]
     public partial interface IAPIEventMessage
     {
     }
