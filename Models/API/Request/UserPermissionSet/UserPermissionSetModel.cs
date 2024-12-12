97ba905b68ba92a89ecc9b28ecbb2e462a45624d
klapsis-netprojects <klapsis@netprojects.gr>
2024-12-12 16:29:35 +0200

    Updates.

--- Models/API/Request/UserPermissionSet/UserPermissionSetModel.cs
+++ Models/API/Request/UserPermissionSet/UserPermissionSetModel.cs
@@ -7,9 +7,15 @@
     public sealed class UserPermissionSetModel : IWebApiModel
     {
         /// <summary>
-        /// Gets or sets the name.
+        /// Gets or sets the Id.
         /// </summary>
         [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [MessagePack.Key(1)]
         public string Name { get; init; } = null!;
     }
 }
