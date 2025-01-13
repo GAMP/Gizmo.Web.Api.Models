6ef21ae79fa83e2495cfdcc3eb0e62afede30bde
Oleg Savelos <olegsavelos@gmail.com>
2025-01-13 16:11:08 +0200

    updates.

--- Models/API/Request/PluginLibrary/PluginLibraryModel.cs
+++ Models/API/Request/PluginLibrary/PluginLibraryModel.cs
@@ -1,23 +1,44 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
 {
+    /// <summary>
+    /// Plugin library model.
+    /// </summary>
     [MessagePack.MessagePackObject()]
-    public sealed class PluginLibraryModel : IWebApiModel
+    public sealed class PluginLibraryModel : IWebApiModel , IModelIntIdentifier
     {
+        /// <inheritdoc/>
         [MessagePack.Key(0)]
         public int Id { get; init; }
 
+        /// <summary>
+        /// File name.
+        /// </summary>
         [MessagePack.Key(1)]
-        public string FileName { get; init; }
+        public string FileName { get; init; } = null!;
 
+        /// <summary>
+        /// Indicates if enabled.
+        /// </summary>
         [MessagePack.Key(2)]
         public bool IsEnabled { get; init; }
 
+        /// <summary>
+        /// Indicates server.
+        /// </summary>
         [MessagePack.Key(3)]
         public bool AvailableInServer { get; init; }
 
+        /// <summary>
+        /// Indicates client.
+        /// </summary>
         [MessagePack.Key(4)]
         public bool AvailableInClient { get; init; }
 
+        /// <summary>
+        /// Indicates manager.
+        /// </summary>
         [MessagePack.Key(5)]
         public bool AvailableInManager { get; init; }
     }
