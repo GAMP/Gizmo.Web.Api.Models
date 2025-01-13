b5de8809d6bc2fd1e26014f396ea34e862271ec1
Oleg Savelos <olegsavelos@gmail.com>
2025-01-13 16:01:30 +0200

    updates.

---
+++ Models/API/Request/PluginLibrary/PluginLibraryModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    [MessagePack.MessagePackObject()]
+    public sealed class PluginLibraryModel : IWebApiModel
+    {
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        [MessagePack.Key(1)]
+        public string FileName { get; init; }
+
+        [MessagePack.Key(2)]
+        public bool IsEnabled { get; init; }
+
+        [MessagePack.Key(3)]
+        public bool AvailableInServer { get; init; }
+
+        [MessagePack.Key(4)]
+        public bool AvailableInClient { get; init; }
+
+        [MessagePack.Key(5)]
+        public bool AvailableInManager { get; init; }
+    }
+}
---
+++ Models/Filters/PluginLibraryFilter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Plugin library filter.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class PluginLibraryFilter : IModelFilter<PluginLibraryModel>
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = new();
+
+        /// <summary>
+        /// Is enabled.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool? IsEnabled { get; init; }
+    }
+}
