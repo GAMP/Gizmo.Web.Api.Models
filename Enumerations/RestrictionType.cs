ea38d6de9b1e00f04037ee1f199f6b941252a456
Oleg Savelos <olegsavelos@gmail.com>
2025-01-22 12:25:50 +0200

    updates.

---
+++ Enumerations/RestrictionType.cs
@@ -0,0 +1,29 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Restriction type.
+    /// </summary>
+    public enum RestrictionType
+    {
+        /// <summary>
+        /// Unset.
+        /// </summary>
+        Unset = 0,
+        /// <summary>
+        /// File name.
+        /// </summary>
+        FileName = 1,
+        /// <summary>
+        /// Window class name.
+        /// </summary>
+        ClassName = 2,
+        /// <summary>
+        /// Window name.
+        /// </summary>
+        WindowName = 3,
+        /// <summary>
+        /// Tray icon.
+        /// </summary>
+        TrayIcon = 4
+    }
+}
