953c2324ff525e5a0bd11b39c612f856b9f32688
klapsis-netprojects <klapsis@netprojects.gr>
2025-01-27 16:14:09 +0200

    Updates.

---
+++ Models/API/Request/Host/Layout/HostsLayoutGridPositionModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    [MessagePack.MessagePackObject()]
+    public sealed class HostsLayoutGridPositionModel : IWebApiModel
+    {
+        [MessagePack.Key(0)]
+        public IEnumerable<HostLayoutGridPositionModel> Hosts { get; set; } = Enumerable.Empty<HostLayoutGridPositionModel>();
+    }
+}
