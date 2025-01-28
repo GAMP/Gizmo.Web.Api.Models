085a1c414d1034f4ef040101a1b962a3bbea57ab
Andrei <masterlifting@Gmail.com>
2025-01-28 01:04:21 +0100

    add IFileModel interface and update FileModel to implement it

---
+++ Abstractions/Models/API/Request/IFileModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// File model.
+    /// </summary>
+    public interface IFileModel : IWebApiModel
+    {
+        /// <summary>
+        /// File name.
+        /// </summary>
+        public string FileName { get; init; }
+        
+        /// <summary>
+        /// File mime type.
+        /// </summary>
+        public string MimeType { get; init; }
+        
+        /// <summary>
+        /// File identifier.
+        /// </summary>
+        public Guid Guid { get; init; }
+    }
+}
--- Models/API/Request/File/FileModel.cs
+++ Models/API/Request/File/FileModel.cs
@@ -1,9 +1,21 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
 {
-    /// <summary>
-    /// File model.
-    /// </summary>
-    public sealed class FileModel : IWebApiModel
+    /// <inheritdoc cref="IFileModel"/>
+    [MessagePack.MessagePackObject]
+    public sealed class FileModel : IFileModel
     {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public string FileName { get; init; } = null!;
+        
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public string MimeType { get; init; } = null!;
+
+        /// <inheritdoc/>
+        [MessagePack.Key(2)]
+        public Guid Guid { get; init; }
     }
 }
