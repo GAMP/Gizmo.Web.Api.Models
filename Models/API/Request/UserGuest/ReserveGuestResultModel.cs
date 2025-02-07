df02686d504d14cc6c34705aa89844b3df6c2f82
Oleg Savelos <olegsavelos@gmail.com>
2025-02-07 17:05:12 +0200

    fix.

--- Models/API/Request/UserGuest/ReserveGuestResultModel.cs
+++ Models/API/Request/UserGuest/ReserveGuestResultModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Gizmo.Web.Api.Models
+﻿namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Reserve guest result model.
@@ -9,51 +7,27 @@ namespace Gizmo.Web.Api.Models
     public sealed class ReserveGuestResultModel : IWebApiModel
     {
         /// <summary>
-        /// Creates new success result instance.
-        /// </summary>
-        /// <param name="userId">User id.</param>
-        /// <param name="slot">Slot number.</param>
-        /// <param name="isNew">New user flag.</param>
-        public ReserveGuestResultModel(int userId, int? slot, bool isNew)
-        {
-            Result = ReserveGuestResult.Success;
-            UserId = userId;
-            Slot = slot;
-            IsNew = isNew;
-        }
-
-        /// <summary>
-        /// Creates new instance.
-        /// </summary>
-        /// <param name="result">Result.</param> 
-        /// <exception cref="ArgumentException">Thrown if <paramref name="result"/> value is equal to <see cref="ReserveGuestResult.Success"/></exception>
-        public ReserveGuestResultModel(ReserveGuestResult result)
-        {
-            Result = result;
-        }
-
-        /// <summary>
         /// Reservation result.
         /// </summary>
         [MessagePack.Key(0)]
-        public ReserveGuestResult Result { get; }
+        public ReserveGuestResult Result { get; init; }
 
         /// <summary>
         /// Indicates that new guest user was created.
         /// </summary>
         [MessagePack.Key(1)]
-        public bool IsNew { get; }
+        public bool IsNew { get; init; }
 
         /// <summary>
         /// Reserved user id.
         /// </summary>
         [MessagePack.Key(2)]
-        public int? UserId { get; }
+        public int? UserId { get; init; }
 
         /// <summary>
         /// Slot.
         /// </summary>
         [MessagePack.Key(3)]
-        public int? Slot {  get; }
+        public int? Slot { get; init; }
     }
 }
