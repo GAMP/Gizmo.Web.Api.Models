b1457af3a63c4357bab3f19951f4db448b28efe7
Oleg Savelos <olegsavelos@gmail.com>
2025-02-11 16:02:51 +0200

    numbers.

---
+++ Models/Messages/EventMessage/UserGuest/IAPIEventMessage.cs
@@ -0,0 +1,11 @@
+﻿using Gizmo.Web.Api.Models;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Messaging
+{
+    [Union(2300, typeof(UserGuestReservedEventMessage))]
+    [Union(2301, typeof(UserGuestReleasedEventMessage))]
+    public partial interface IAPIEventMessage
+    {
+    }
+}
