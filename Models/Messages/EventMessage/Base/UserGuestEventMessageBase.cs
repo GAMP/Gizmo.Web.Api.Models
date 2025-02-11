997b3a253ac6bf494a5078f62d8e757773319611
Oleg Savelos <olegsavelos@gmail.com>
2025-02-11 15:39:09 +0200

    add user guest events.

---
+++ Models/Messages/EventMessage/Base/UserGuestEventMessageBase.cs
@@ -0,0 +1,23 @@
+﻿using Gizmo.Web.Api.Messaging;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User guest event message base.
+    /// </summary>
+    [System.ComponentModel.DataAnnotations.Name("User guest", "USER_GUEST_EVENT_GROUP_NAME")]
+    [System.ComponentModel.DataAnnotations.ExtendedDescription("User guest related events", "USER_GUEST_EVENT_GROUP_DESCRIPTION")]
+    [EventGroup(23)]
+    public abstract class UserGuestEventMessageBase : APIEventMessage
+    {
+        /// <inheritdoc/>
+        public UserGuestEventMessageBase()
+        { }
+
+        /// <summary>
+        /// User id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int UserId { get; init; }
+    }
+}
---
+++ Models/Messages/EventMessage/UserGuest/UserGuestReleasedEventMessage.cs
@@ -0,0 +1,27 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User guest released event message.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserGuestReleasedEventMessage : UserGuestEventMessageBase
+    {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public UserGuestReleasedEventMessage() : base()
+        { }
+
+        /// <summary>
+        /// Host id.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int? HostId { get; init; }
+
+        /// <summary>
+        /// Slot id.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public int? Slot { get; init; }
+    }
+}
