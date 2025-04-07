340d2c569694cd951c6ffa713550dfb0638405c1
Oleg Savelos <olegsavelos@gmail.com>
2025-04-07 11:16:07 +0300

    fix for orphaned usage entities.

---
+++ Models/API/Request/User/UsageSession/User/UsageUnknownModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Active usage unknown model.
+    /// </summary>
+    /// <remarks>
+    /// This model serves as a proxy for an cases where there are orphaned records in the database and helps to avoid query errors.<br></br>
+    /// The orphaned records should not be common but can be found in some databases.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class UsageUnknownModel : UsageModel
+    {
+    }
+}
