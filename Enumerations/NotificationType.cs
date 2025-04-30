36cca8c5a412e810dd8d76d530c9127b841aa65e
klapsis-netprojects <klapsis@netprojects.gr>
2025-04-30 17:07:03 +0300

    Updates.

--- Enumerations/NotificationType.cs
+++ Enumerations/NotificationType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gizmo.Web.Api.Models
 {
@@ -11,18 +12,25 @@ namespace Gizmo.Web.Api.Models
         /// <summary>
         /// None.
         /// </summary>
+        [Name("None", "NOTIFICATION_TYPE_NONE")]
         None = 0,
+
         /// <summary>
         /// Visual.
         /// </summary>
+        [Name("Visual", "NOTIFICATION_TYPE_VISUAL")]
         Visual = 1,
+
         /// <summary>
         /// Audible.
         /// </summary>
+        [Name("Audible", "NOTIFICATION_TYPE_AUDIBLE")]
         Audible = 2,
+
         /// <summary>
         /// Visual and Audible.
         /// </summary>
+        [Name("Visual And Audible", "NOTIFICATION_TYPE_VISUAL_AND_AUDIBLE")]
         VisualAndAudible = Visual | Audible,
     }
 }
--- Enumerations/StockType.cs
+++ Enumerations/StockType.cs
@@ -1,4 +1,6 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Stock type.
@@ -8,11 +10,13 @@
         /// <summary>
         /// Selling point.
         /// </summary>
+        [Name("Selling Point", "STOCK_TYPE_SELLING_POINT")]
         SellingPoint = 0,
 
         /// <summary>
         /// Warehouse.
         /// </summary>
+        [Name("Warehouse", "STOCK_TYPE_WAREHOUSE")]
         Warehouse = 1
     }
 }
