64544e4f72d6f4f16ca1c898e61e10b799a0e669
Oleg Savelos <olegsavelos@gmail.com>
2025-05-12 17:50:23 +0300

    updates.

---
+++ Models/API/Request/DiscountGroup/DiscountGroupModel.cs
@@ -0,0 +1,21 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Discount group model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class DiscountGroupModel : IWebApiModel, IModelIntIdentifier
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Discount group name.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string Name { get; init; } = null!;
+    }
+}
---
+++ Models/Filters/DiscountGroupFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Discount group filter.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class DiscountGroupFilter : IModelFilter<DiscountGroupModel>
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = new();
+    }
+}
