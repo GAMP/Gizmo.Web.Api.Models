d48ecf1acc7a9a669bc5499531a6b8ffba17b5f0
klapsis-netprojects <klapsis@netprojects.gr>
2025-05-12 15:39:58 +0300

    Updates.

--- Enumerations/RestrictionType.cs
+++ Enumerations/RestrictionType.cs
@@ -1,4 +1,6 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Restriction type.
@@ -8,22 +10,31 @@
         /// <summary>
         /// Unset.
         /// </summary>
+        [Name("Unset", "RESTRICTION_TYPE_UNSET")]
         Unset = 0,
+
         /// <summary>
         /// File name.
         /// </summary>
+        [Name("File Name", "RESTRICTION_TYPE_FILE_NAME")]
         FileName = 1,
+
         /// <summary>
         /// Window class name.
         /// </summary>
+        [Name("Class Name", "RESTRICTION_TYPE_CLASS_NAME")]
         ClassName = 2,
+
         /// <summary>
         /// Window name.
         /// </summary>
+        [Name("Window Name", "RESTRICTION_TYPE_WINDOW_NAME")]
         WindowName = 3,
+
         /// <summary>
         /// Tray icon.
         /// </summary>
+        [Name("Tray Icon", "RESTRICTION_TYPE_TRAY_ICON")]
         TrayIcon = 4
     }
 }
