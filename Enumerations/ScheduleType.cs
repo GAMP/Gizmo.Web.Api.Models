b9f4590402c008bb30eae38636e341254b40ca61
klapsis-netprojects <klapsis@netprojects.gr>
2025-05-20 09:51:27 +0300

    Updates.

--- Enumerations/ScheduleType.cs
+++ Enumerations/ScheduleType.cs
@@ -1,4 +1,6 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Schedule type.
@@ -8,22 +10,31 @@
         /// <summary>
         /// Daily schedule.
         /// </summary>
+        [Name("Daily", "SCHEDULE_TYPE_DAILY")]
         Daily = 0,
+
         /// <summary>
         /// Weekly schedule.
         /// </summary>
+        [Name("Weekly", "SCHEDULE_TYPE_WEEKLY")]
         Weekly = 1,
+
         /// <summary>
         /// Monthly schedule.
         /// </summary>
+        [Name("Monthly", "SCHEDULE_TYPE_MONTHLY")]
         Monthly = 2,
+
         /// <summary>
         /// Yearly schedule.
         /// </summary>
+        [Name("Yearly", "SCHEDULE_TYPE_YEARLY")]
         Yearly = 3,
+
         /// <summary>
         /// One time schedule.
         /// </summary>
+        [Name("One Time", "SCHEDULE_TYPE_ONE_TIME")]
         OneTime = 4
     }
 }
