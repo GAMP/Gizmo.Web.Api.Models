9f78ad8e7896ba945717bd11a8d6cba2767b7765
Oleg Savelos <olegsavelos@gmail.com>
2025-05-29 15:54:39 +0300

    updates.

---
+++ Models/API/Request/UserGuest/UserGuestVirtualResult.cs
@@ -0,0 +1,28 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User guest virtual result model.
+    /// </summary>
+
+    [MessagePack.MessagePackObject()]
+    public sealed class UserGuestVirtualResult : IWebApiModel
+    {
+        /// <summary>
+        /// Reserved guest user Id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? UserId { get; init; }
+
+        /// <summary>
+        /// Host id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int HostId { get; init; }
+
+        /// <summary>
+        /// Slot number.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int Slot { get; init; }
+    }
+}
---
+++ Models/Filters/UserGuestVirtualFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Virtual user guest search filter model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserGuestVirtualFilter
+    {
+        /// <summary>
+        /// Results limit.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? Limit { get; init; }
+
+        /// <summary>
+        /// Branch id to filter by.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int? BranchId { get; set; }
+
+        /// <summary>
+        /// Search value.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public string? SearchValue { get; init; }
+    }
+}
