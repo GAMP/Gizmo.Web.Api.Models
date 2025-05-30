6506dd0ae45110098fc38acdd0bb0166e6eb22af
Oleg Savelos <olegsavelos@gmail.com>
2025-05-30 12:20:45 +0300

    search updates.

---
+++ Models/API/Request/User/Search/UserSearchResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User search result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserSearchResult : IWebApiModel
+    {
+        /// <summary>
+        /// User id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int UserId { get; set; }
+    }
+}
---
+++ Models/Filters/UserSearchFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{    /// <summary>
+     /// User search filter model.
+     /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserSearchFilter : IModelFilter<UserSearchResult>
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public ModelFilterPagination Pagination { get; set; } = new();
+
+        /// <inheritdoc/>
+        [MessagePack.Key(1)]
+        public List<string> Expand { get; set; } = new();
+
+        /// <summary>
+        /// Search value to filter users by.<br></br>
+        /// </summary>
+        [MessagePack.Key(2)]
+        [Required()]
+        public string SearchValue { get; set; } = null!;
+    }
+}
