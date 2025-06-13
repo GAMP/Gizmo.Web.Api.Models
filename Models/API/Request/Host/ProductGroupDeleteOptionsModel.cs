c16b16429156d301a06768c0de83e4c91e6ba5d7
klapsis-netprojects <klapsis@netprojects.gr>
2025-06-13 11:28:30 +0300

    Updates.

---
+++ Models/API/Request/Host/ProductGroupDeleteOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product group delete options.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ProductGroupDeleteOptionsModel : IUriParametersQuery
+    {
+        /// <summary>
+        /// The id of the product group to move products before delete.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? MoveToProductGroupId { get; init; }
+    }
+}
---
+++ Models/API/Request/Host/ProductGroupDeleteResultModel.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product group delete result.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ProductGroupDeleteResultModel : IWebApiModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Product group delete result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ProductGroupDeleteResult ProductGroupDeleteResult { get; init; }
+
+        #endregion
+    }
+}
