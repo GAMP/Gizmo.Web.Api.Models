3b12f930e9f78272d65564d79fd159321f3dc4f1
klapsis-netprojects <klapsis@netprojects.gr>
2025-06-16 11:01:05 +0300

    Updates.

---
+++ Models/API/Request/Product/ProductGroupCountersModel.cs
@@ -0,0 +1,33 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product group counters.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ProductGroupCountersModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Product group Id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int ProductGroupId { get; set; }
+
+        /// <summary>
+        /// Total products within product group.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int TotalProducts { get; set; }
+
+        /// <summary>
+        /// Total deleted products within user group.
+        /// </summary>
+        [MessagePack.Key(4)]
+        public int DeletedProducts { get; set; }
+
+        #endregion
+    }
+}
---
+++ Models/API/Request/Product/ProductsCountersModel.cs
@@ -0,0 +1,36 @@
+﻿using MessagePack;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Products counters.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class ProductsCountersModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// Counters per product group.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<ProductGroupCountersModel> CountersPerGroup { get; set; } = Enumerable.Empty<ProductGroupCountersModel>();
+
+        /// <summary>
+        /// Total products.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int TotalProducts { get; set; }
+
+        /// <summary>
+        /// Deleted products.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public int DeletedProducts { get; set; }
+
+        #endregion
+    }
+}
