66b4ad5e37a14dc6880876646e3e7ea212aa7cb7
Oleg Savelos <olegsavelos@gmail.com>
2025-06-19 14:41:42 +0300

    updates.

--- Models/API/Request/Stock/Count/StockCountEntryModelCreate.cs
+++ Models/API/Request/Stock/Count/StockCountEntryModelCreate.cs
@@ -7,12 +7,15 @@
     public sealed class StockCountEntryModelCreate : IWebApiModel, IStockCountEntryModel
     {
         /// <inheritdoc/>
+        [MessagePack.Key(0)]
         public int ProductId { get; init; }
 
         /// <inheritdoc/>
+        [MessagePack.Key(1)]
         public decimal Actual { get; init; }
 
         /// <inheritdoc/>
+        [MessagePack.Key(2)]
         public string? Note { get; init; }
     }
 }
