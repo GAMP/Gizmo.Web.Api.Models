41e47dda4fc25f150679c256de00a59cd0e407d2
klapsis-netprojects <klapsis@netprojects.gr>
2025-07-04 10:49:24 +0300

    Updates.

--- Enumerations/InventoryType.cs
+++ Enumerations/InventoryType.cs
@@ -1,4 +1,6 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Inventory type.
@@ -8,14 +10,17 @@
         /// <summary>
         /// Inbound.
         /// </summary>
+        [Name("Inbound", "INVENTORY_TYPE_INBOUND")]
         Inbound = 0,
         /// <summary>
         /// Transfer.
         /// </summary>
+        [Name("Transfer", "INVENTORY_TYPE_TRANSFER")]
         Transfer = 1,
         /// <summary>
         /// Adjustment.
         /// </summary>
+        [Name("Adjustment", "INVENTORY_TYPE_ADJUSTMENT")]
         Adjustment = 2
     }
 }
