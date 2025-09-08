f6bbb8e1f64321838da8915c65b1aceec117ab28
Oleg Savelos <olegsavelos@gmail.com>
2025-09-08 11:36:22 +0300

    models.

---
+++ Models/API/Request/Cart/CartClearResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart clear result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartClearResultModel : IWebApiModel
+    {
+    }
+}
---
+++ Models/API/Request/Cart/CartDeleteResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart deletion result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartDeleteResultModel : IWebApiModel
+    {
+    }
+}
