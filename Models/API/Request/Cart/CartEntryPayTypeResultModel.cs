4b32737ed7c6f451b11c19f1becccb212bdd0b1b
Oleg Savelos <olegsavelos@gmail.com>
2025-09-08 18:37:55 +0300

    models.

---
+++ Models/API/Request/Cart/CartEntryPayTypeResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart entry pay type result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartEntryPayTypeResultModel : IWebApiModel
+    {
+    }
+}
---
+++ Models/API/Request/Cart/CartEntryPriceResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart entry price result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartEntryPriceResultModel : IWebApiModel
+    {
+    }
+}
