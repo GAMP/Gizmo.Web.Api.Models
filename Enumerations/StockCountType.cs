d96835c660b23360d1e85cb2f792e7abe03e8e4e
klapsis-netprojects <klapsis@netprojects.gr>
2025-09-10 10:36:27 +0300

    Updates.

--- Enumerations/StockCountType.cs
+++ Enumerations/StockCountType.cs
@@ -1,12 +1,19 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Stock count type.
     /// </summary>
     public enum StockCountType
     {
+        [Name("Default", "SHIFT_COUNT_TYPE_DEFAULT")]
         Default = 0,
+
+        [Name("Shift Open", "SHIFT_COUNT_TYPE_SHIFT_OPEN")]
         ShiftOpen = 1,
+
+        [Name("Shift Close", "SHIFT_COUNT_TYPE_SHIFT_CLOSE")]
         ShiftClose = 2,
     }
 }
