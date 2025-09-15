cba7a105faf78ffef47c4f2be06519dee589ab4c
Oleg Savelos <olegsavelos@gmail.com>
2025-09-15 18:39:24 +0300

    cart updates.

--- Models/API/Request/Cart/CartEntryAddResultModel.cs
+++ Models/API/Request/Cart/CartEntryAddResultModel.cs
@@ -8,6 +8,9 @@ namespace Gizmo.Web.Api.Models
     [MessagePack.MessagePackObject()]
     public sealed class CartEntryAddResultModel : IWebApiModel
     {
+        /// <summary>
+        /// Entry id.
+        /// </summary>
         [MessagePack.Key(0)]
         public Guid Id { get; init; }
     }
---
+++ Models/API/Request/Cart/CartEntryFixedTimeAddModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart entry fixed time addition model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartEntryFixedTimeAddModel : IWebApiModel
+    {
+        /// <summary>
+        /// Fixed time minutes.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Minutes { get; set; }
+
+        /// <summary>
+        /// Host group id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int HostGroupId { get; set; }
+
+        /// <summary>
+        /// Start date.
+        /// </summary>
+        /// <remarks>
+        /// This value is required to calculate totals based on bill profile.
+        /// </remarks>
+        [MessagePack.Key(2)]
+        public DateTime StartDate { get; set; }
+    }
+}
