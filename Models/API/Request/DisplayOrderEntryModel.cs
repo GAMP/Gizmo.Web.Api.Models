5fdd7cd70bc4653e6c9610d6a379ddbaf35e84d6
klapsis-netprojects <klapsis@netprojects.gr>
2025-09-19 15:51:59 +0300

    Add display order endpoint.

---
+++ Models/API/Request/DisplayOrderEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Display order entry.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class DisplayOrderEntryModel
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The Id of the object.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// The display order of the entry.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public int DisplayOrder { get; set; }
+
+        #endregion
+    }
+}
---
+++ Models/API/Request/DisplayOrderModelUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Display order.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class DisplayOrderModelUpdate
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// The name of the product group.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<DisplayOrderEntryModel> Entries { get; set; } = Enumerable.Empty<DisplayOrderEntryModel>();
+
+        #endregion
+    }
+}
