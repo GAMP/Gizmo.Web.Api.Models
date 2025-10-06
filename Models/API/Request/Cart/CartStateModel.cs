5645fe3fbb522826b00010dc41dc9cfa9a16cf14
Oleg Savelos <olegsavelos@gmail.com>
2025-10-06 13:40:20 +0300

    model updates.

--- Models/API/Request/Cart/CartStateModel.cs
+++ Models/API/Request/Cart/CartStateModel.cs
@@ -92,6 +92,12 @@ namespace Gizmo.Web.Api.Models
         /// </summary>
         [MessagePack.Key(2)]
         public IEnumerable<CartPaymentStateModel> Payments { get; init; } = Enumerable.Empty<CartPaymentStateModel>();
+
+        /// <summary>
+        /// Gets minimum payment amount.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public decimal MinimumPayment {  get; init; }
     }
 
     /// <summary>
