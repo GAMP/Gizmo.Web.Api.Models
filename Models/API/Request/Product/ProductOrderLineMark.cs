71abd9f4673e48f4b59d13b1edc4eab5f53c4b17
Oleg Savelos <olegsavelos@gmail.com>
2025-10-14 12:04:57 +0300

    mark model.

---
+++ Models/API/Request/Product/ProductOrderLineMark.cs
@@ -0,0 +1,24 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Product order line mark model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class ProductOrderLineMarkModel : IWebApiModel
+    {
+        /// <summary>
+        /// Product order line id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int ProductOrderLineId { get; init; }
+
+        /// <summary>
+        /// Mark.
+        /// </summary>
+        /// <remarks>
+        /// The value can be null to clear the mark.
+        /// </remarks>
+        [MessagePack.Key(1)]
+        public string? Mark { get; init; }
+    }
+}
