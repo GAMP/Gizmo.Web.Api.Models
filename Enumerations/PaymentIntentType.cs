6e1fbbc41dc82028c9490163952ba32ae171cc1c
Oleg Savelos <olegsavelos@gmail.com>
2025-10-21 15:02:17 +0300

    payment intent updates.

---
+++ Enumerations/PaymentIntentType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payment intent type.
+    /// </summary>
+    /// <remarks>
+    /// The flag is used to represent multiple intent types in a single value.
+    /// </remarks>
+    [Flags]
+    public enum PaymentIntentType
+    {
+        /// <summary>
+        /// Deposit.
+        /// </summary>
+        Deposit = 1,
+
+        /// <summary>
+        /// Order.
+        /// </summary>
+        Order = 2,
+
+        /// <summary>
+        /// Invoice.
+        /// </summary>
+        Invoice = 4
+    }
+}
