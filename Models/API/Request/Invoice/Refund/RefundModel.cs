f88ddb0b568a892652570566bb7e6df897165812
Oleg Savelos <olegsavelos@gmail.com>
2025-10-24 16:43:43 +0300

    updates.

---
+++ Models/API/Request/Invoice/Refund/RefundModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Refund model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class RefundModel : IWebApiModel
+    {
+        /// <summary>
+        /// Refund payment method method.
+        /// </summary>
+        /// <remarks>Set to <see langword="null"/> to indicate original tender refund.</remarks>
+        [MessagePack.Key(0)]
+        public int? RefundMethodId { get; init; }
+
+        /// <summary>
+        /// Refund amount.
+        /// </summary>
+        /// <remarks>
+        /// This value is only used if <see cref="RefundMethodId"/> is specified.
+        /// </remarks>
+        [MessagePack.Key(1)]
+        public decimal RefundAmount { get; init; }
+
+        /// <summary>
+        /// Disable receipt printing.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool DisableReceiptPrinting { get; init; }
+    }
+}
---
+++ Models/API/Request/Invoice/Refund/RefundStateModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Refund state model.
+    /// </summary>
+    /// <remarks>
+    /// Provides information on possible refund method and amounts.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class RefundStateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Maximum refund amount.
+        /// </summary>
+        /// <remarks>
+        /// This value will indicate the maximum amount that can be refunded.<br></br>
+        /// In most cases it will be the sum of all payments made, in some cases such as with deposits it can be less since <b>part of deposit could have been spent already</b>.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public decimal MaxRefundAmount { get; init; }
+
+        /// <summary>
+        /// Refund methods.
+        /// </summary>
+        /// <remarks>
+        /// When <see cref="RequireOriginalTender"/> will be equal to <see langword="true"/> this collection will contain all the methods that will be used for refund, when <see langword="false"/> this will contain the methods that can be used.
+        /// </remarks>
+        [MessagePack.Key(1)]
+        public IEnumerable<int> RefundMethods { get; set; } = Enumerable.Empty<int>();
+
+        /// <summary>
+        /// Indicates original tender.
+        /// </summary>
+        /// <remarks>
+        /// When <see langword="true"/> this will indicate that all the payments identified by <see cref="RefundMethods"/> will be fully reversed (refunded).<br></br>
+        /// The value of <see cref="MaxRefundAmount"/> will be equal to sum of the payments.
+        /// </remarks>
+        [MessagePack.Key(2)]
+        public bool RequireOriginalTender { get; init; }
+    }
+}
