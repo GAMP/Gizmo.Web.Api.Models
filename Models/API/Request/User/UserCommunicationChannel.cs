c16d676fdcc8d020063f9babf9003e30183bdd0a
Oleg Savelos <olegsavelos@gmail.com>
2025-11-24 10:56:26 +0200

    user channel model.

---
+++ Models/API/Request/User/UserCommunicationChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User communication channel.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class UserCommunicationChannel : IWebApiModel
+    {
+        /// <summary>
+        /// Channel type.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public Guid Type { get; init; }
+
+        /// <summary>
+        /// Channel value.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public string Value { get; set; } = null!;
+
+        /// <summary>
+        /// Indicates that channel is verified.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool IsVerified { get; set; }
+    }
+}
