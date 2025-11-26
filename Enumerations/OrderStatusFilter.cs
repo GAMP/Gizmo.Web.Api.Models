c02bef38e258fe02e17b6ff6e406a9df3f3e5fb6
klapsis-netprojects <klapsis@netprojects.gr>
2025-11-26 15:57:08 +0200

    Updates.

--- Enumerations/OrderStatusFilter.cs
+++ Enumerations/OrderStatusFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gizmo.Web.Api.Models
 {
@@ -12,26 +13,31 @@ namespace Gizmo.Web.Api.Models
         /// <summary>
         /// On hold.
         /// </summary>
+        [Name("On Hold", "ORDER_STATUS_FILTER_ON_HOLD")]
         OnHold = 1,
 
         /// <summary>
         /// Completed.
         /// </summary>
+        [Name("Completed", "ORDER_STATUS_FILTER_COMPLETED")]
         Completed = 2,
 
         /// <summary>
         /// Canceled.
         /// </summary>
+        [Name("Canceled", "ORDER_STATUS_FILTER_CANCELED")]
         Canceled = 4,
 
         /// <summary>
         /// Accepted.
         /// </summary>
+        [Name("Accepted", "ORDER_STATUS_FILTER_ACCEPTED")]
         Accepted = 8,
 
         /// <summary>
         /// Processing.
         /// </summary>
+        [Name("Processing", "ORDER_STATUS_FILTER_PROCESSING")]
         Processing = 16,
     }
 }
