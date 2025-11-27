7429369bf85240c8f42b2d6e24224377f803adfd
Oleg Savelos <olegsavelos@gmail.com>
2025-11-27 15:10:32 +0200

    query.

--- Models/API/Request/Host/Computer/SystemProcessFilterModel.cs
+++ Models/API/Request/Host/Computer/SystemProcessFilterModel.cs
@@ -1,10 +1,12 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Process filter.
     /// </summary>
     [MessagePack.MessagePackObject()]
-    public sealed class SystemProcessFilterModel : IWebApiModel
+    public sealed class SystemProcessFilterModel : IWebApiModel, IUriParametersQuery
     {
         /// <summary>
         /// Optional process name.
