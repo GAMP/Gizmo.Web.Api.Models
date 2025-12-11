7499787528f62e14625da6d201e00d85f9a34739
Oleg Savelos <olegsavelos@gmail.com>
2025-12-11 14:02:53 +0200

    updates.

---
+++ Models/API/Request/Payment/Intent/PaymentIntentOrderModel.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payment intent order model.
+    /// </summary>
+    /// <remarks>
+    /// Describes order associated with intent, this model can represent both order and deposit intents.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentIntentOrderModel : IWebApiModel
+    {
+        /// <summary>
+        /// Order intents.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<PaymentIntentOrderOrderModel> Orders { get; init; } = Enumerable.Empty<PaymentIntentOrderOrderModel>();
+
+        /// <summary>
+        /// Deposit intents.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public IEnumerable<PaymentIntentOrderDepositModel> Deposits { get; init; } = Enumerable.Empty<PaymentIntentOrderDepositModel>();
+    }
+
+    /// <summary>
+    /// Payment intent order order model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentIntentOrderOrderModel : IWebApiModel
+    {
+        /// <summary>
+        /// Product order id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int OrderId { get; init; }
+
+        /// <summary>
+        /// Invoices.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public IEnumerable<PaymentIntentOrderInvoiceModel> Invoices { get; set; } = Enumerable.Empty<PaymentIntentOrderInvoiceModel>();
+    }
+
+    /// <summary>
+    /// Payment intent order invoice model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentIntentOrderInvoiceModel : IWebApiModel
+    {
+        /// <summary>
+        /// Invoice id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int InvoiceId { get; init; }
+
+        /// <summary>
+        /// Receipt print status.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public Gizmo.FiscalReceiptPrintStatus ReceiptPrintStatus { get; init; }
+    }
+
+    /// <summary>
+    /// Payment intent order deposit model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentIntentOrderDepositModel : IWebApiModel
+    {
+        /// <summary>
+        /// Deposit payment.
+        /// </summary>
+        /// <remarks>
+        /// Will be null if no payment is associated with intent.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public PaymentIntentOrderDepositPaymentModel? DepositPayment { get; init; }
+    }
+
+    /// <summary>
+    /// Payment intent order deposit deposit payment model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentIntentOrderDepositPaymentModel : IWebApiModel
+    {
+        /// <summary>
+        /// Deposit payment id.
+        /// </summary>
+        /// <remarks>
+        /// Null means that no deposit payment is associated with deposit intent.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public int DepositPaymentId { get; init; }
+
+        /// <summary>
+        /// Receipt print status.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public Gizmo.FiscalReceiptPrintStatus ReceiptPrintStatus { get; init; }
+    }
+}
