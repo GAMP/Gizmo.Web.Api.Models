691154d43b914a4948020ee700a3321278f2185e
Oleg Savelos <olegsavelos@gmail.com>
2025-12-15 10:21:42 +0200

    order intent payment  result model.

---
+++ Models/API/Request/Order/OrderIntentPaymentsCreateResult.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Order intent payments create result.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class OrderIntentPaymentsCreateResult : IWebApiModel
+    {
+        /// <summary>
+        /// Created payments.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<AcceptedOrderPaymentModel> Payments { get; set; } = Enumerable.Empty<AcceptedOrderPaymentModel>();
+
+        /// <summary>
+        /// Created invoices.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public IEnumerable<AcceptedOrderInvoiceModel> Invoices { get; set; } = Enumerable.Empty<AcceptedOrderInvoiceModel>();
+
+        /// <summary>
+        /// Processed deposit payments.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public IEnumerable<AcceptedDepositPaymentModel> DepositPayments { get; init; } = Enumerable.Empty<AcceptedDepositPaymentModel>();
+
+        /// <summary>
+        /// Expected payment.
+        /// </summary>
+        [MessagePack.Key(3)]
+        public ExpectedOrderPaymentModel? ExpectedPayment { get; init; }
+    }
+}
