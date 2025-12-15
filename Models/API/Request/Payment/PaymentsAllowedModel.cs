ce1bd0abffe3bb966612aff07fed861de42904b8
Oleg Savelos <olegsavelos@gmail.com>
2025-12-15 10:48:10 +0200

    model.

---
+++ Models/API/Request/Payment/PaymentsAllowedModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payments allowed model.
+    /// </summary>
+    /// <remarks>
+    /// Represents a request model for checking allowed payment methods.
+    /// </remarks>
+    [MessagePack.MessagePackObject()]
+    public sealed class PaymentsAllowedModel : IWebApiModel
+    {
+        /// <summary>
+        /// Allowed payment methods.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public IEnumerable<int> AllowedPaymentMethods { get; init; } = Enumerable.Empty<int>();
+
+        /// <summary>
+        /// Indicates if multi payment methods are allowed.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public bool MultiMethodAllowed { get; init; }
+    }
+}
