2240b8144319dac406b54ebd558f2e836d55c773
Oleg Savelos <olegsavelos@gmail.com>
2025-12-22 14:43:49 +0200

    updates.

---
+++ Models/API/Request/Payment/Payments/PaymentReversResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payment reversal result model.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class PaymentReversResultModel : IWebApiModel
+    {
+    }
+}
---
+++ Models/API/Request/Payment/Payments/PaymentReversalModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Payment reversal model.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class PaymentReversalModel : IWebApiModel
+    {
+        /// <summary>
+        /// Indicates an void.
+        /// </summary>
+        /// <remarks>
+        /// Setting this to true indicates that the payment recipients will be voided.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public bool Void { get; init; }
+    }
+}
