410db6646279e09ca646172fc0e347095adb565e
Oleg Savelos <olegsavelos@gmail.com>
2026-01-05 12:44:25 +0200

    new models.

---
+++ Models/API/Request/Cart/CartPaymentMethodSetModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart payment method set model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartPaymentMethodSetModel : IWebApiModel
+    {
+        /// <summary>
+        /// Payment method id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int PaymentMethodId { get; set; }
+    }
+}
---
+++ Models/API/Request/Cart/CartPaymentMethodSetResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Cart payment method set result model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class CartPaymentMethodSetResultModel : IWebApiModel
+    {
+    }
+}
