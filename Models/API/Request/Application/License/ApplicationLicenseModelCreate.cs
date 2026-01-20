1bd7bae556019529c5e396d37cec170f32702e6d
Oleg Savelos <olegsavelos@gmail.com>
2026-01-20 12:13:30 +0200

    models updates.

--- Models/API/Request/Application/License/ApplicationLicenseModelCreate.cs
+++ Models/API/Request/Application/License/ApplicationLicenseModelCreate.cs
@@ -18,28 +18,18 @@ namespace Gizmo.Web.Api.Models
         [MessagePack.Key(0)]
         [StringLength(255)]
         [Required()]
-        public string Name { get; set; } = null!;
+        public required string Name { get; set; }
 
         /// <summary>
-        /// The plugin type name of the license.
+        /// License plugin unique identifier.
         /// </summary>
         [MessagePack.Key(1)]
-        [StringLength(255)]
-        [Required()]
-        public string Plugin { get; set; } = null!;
-
-        /// <summary>
-        /// The plugin assembly of the license.
-        /// </summary>
-        [MessagePack.Key(2)]
-        [StringLength(255)]
-        [Required()]
-        public string Assembly { get; set; } = null!;
+        public required Guid Plugin { get; set; }
 
         /// <summary>
         /// License keys.
         /// </summary>
-        [MessagePack.Key(3)]
+        [MessagePack.Key(2)]
         public required IEnumerable< ApplicationLicenseKeyCrateUpdateModel> Keys { get; init; }
     }
 }
--- Models/API/Request/Application/License/ApplicationLicenseModelUpdate.cs
+++ Models/API/Request/Application/License/ApplicationLicenseModelUpdate.cs
@@ -3,6 +3,7 @@
 using MessagePack;
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Gizmo.Web.Api.Models
@@ -31,6 +32,6 @@ namespace Gizmo.Web.Api.Models
         /// License keys.
         /// </summary>
         [MessagePack.Key(2)]
-        public required ApplicationLicenseKeyCrateUpdateModel Keys { get; init; }
+        public required IEnumerable<ApplicationLicenseKeyCrateUpdateModel> Keys { get; init; }
     }
 }
