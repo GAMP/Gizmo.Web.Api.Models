b3e0d8273e665064046b3af3ca2c104009b31b71
Oleg Savelos <olegsavelos@gmail.com>
2026-01-20 13:13:52 +0200

    updates.

---
+++ Models/API/Request/Application/License/ApplicationLicenseSetHostIdModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Set host id model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class ApplicationLicenseSetHostIdModel : IWebApiModel
+    {
+        /// <summary>
+        /// Host id.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int? HostId { get; init; }
+    }
+}
