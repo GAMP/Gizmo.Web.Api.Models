aa0697be795adda5df64ddd06165d537e250d791
Oleg Savelos <olegsavelos@gmail.com>
2026-01-20 08:18:07 +0200

    smart card model.

---
+++ Models/API/Request/User/UserSetSmartCardUidModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Set user smart card UID model.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class UserSetSmartCardUidModel : IWebApiModel
+    {
+        /// <summary>
+        /// Smart card UID.
+        /// </summary>
+        /// <remarks>
+        /// Setting to null or empty string will remove the smart card association.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public string? SmartCardUid { get; set; } = null!;
+    }
+}
