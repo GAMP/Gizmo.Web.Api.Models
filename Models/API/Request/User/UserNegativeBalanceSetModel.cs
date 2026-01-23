b1fec9ebd20823a33e8b9b552158c86d78d836ee
Oleg Savelos <olegsavelos@gmail.com>
2026-01-23 13:39:44 +0200

    add negative balance support.

---
+++ Models/API/Request/User/UserNegativeBalanceSetModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User negative balance set model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public class UserNegativeBalanceSetModel : IWebApiModel
+    {
+        /// <summary>
+        /// Enables user negative balance.
+        /// </summary>
+        /// <remarks>
+        /// Leave null to fallback to user group settings, set value to override it.
+        /// </remarks>
+        [MessagePack.Key(0)]
+        public bool? IsEnabled { get; set; }
+    }
+}
---
+++ Models/Messages/EventMessage/User/UserNegativeBalanceEnabledMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Messaging
+{
+    /// <summary>
+    /// User negative balance enabled event message.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserNegativeBalanceEnabledMessage : UserEventMessageBase
+    {
+        /// <summary>
+        /// Indicates if negative balance is enabled.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public bool? IsEnabled { get; init; }
+    }
+}
