5eb57efc5981923ffa639497976694f2ea2f4115
Oleg Savelos <olegsavelos@gmail.com>
2026-01-25 10:30:59 +0200

    updates.

---
+++ Enumerations/InvoiceStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Invoice status.
+    /// </summary>
+    public enum InvoiceStatus
+    {
+        /// <summary>
+        /// Unpaid.
+        /// </summary>
+        Unpaid = 0,
+
+        /// <summary>
+        /// Partially paid.
+        /// </summary>
+        PartiallyPaid = 1,
+
+        /// <summary>
+        /// Paid.
+        /// </summary>
+        Paid = 2,
+    }
+}
---
+++ Enumerations/LogoutResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Logout result.
+    /// </summary>
+    [Flags()]
+    public enum LogoutResult
+    {
+        /// <summary>
+        /// Logout was successful.
+        /// </summary>
+        Success = 0,
+
+        /// <summary>
+        /// User is not logged in.
+        /// </summary>
+        NotLoggedIn = 1,
+
+        /// <summary>
+        /// Logout occurred locally.
+        /// </summary>
+        LoggedOutLocally = 2,
+
+        /// <summary>
+        /// Invalid user id.
+        /// </summary>
+        InvalidUserId = 2048,
+
+        /// <summary>
+        /// Logout failed.
+        /// </summary>
+        Failed = 4,
+    }
+}
