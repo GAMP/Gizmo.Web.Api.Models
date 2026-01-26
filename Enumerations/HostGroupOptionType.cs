8a9f2952772fedad3f44a73ed61a5738729f2829
Oleg Savelos <olegsavelos@gmail.com>
2026-01-26 11:31:46 +0200

    updates.

---
+++ Enumerations/HostGroupOptionType.cs
@@ -0,0 +1,13 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Host group options.
+    /// </summary>
+    public enum HostGroupOptionType
+    {
+        /// <summary>
+        /// None.
+        /// </summary>
+        None = 0,
+    }
+}
