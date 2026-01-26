388650d8416d0c474a57d1a927ff0e348e36ebd3
Oleg Savelos <olegsavelos@gmail.com>
2026-01-26 20:26:41 +0200

    updates.

---
+++ Models/API/Request/Installer/InstallerModuleInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Installer module info.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public class InstallerModuleInfo : IWebApiModel
+    {
+        /// <summary>
+        /// Full path.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public required string FullPath
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Version.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public System.Version? Version
+        {
+            get; set;
+        } 
+    }
+}
