40dd4eab28c76bf719d5d2cb2d84205bec53dfea
Oleg Savelos <olegsavelos@gmail.com>
2026-01-26 16:03:51 +0200

    updates.

---
+++ Enumerations/OrderOptionType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Order options.
+    /// </summary>
+    [Flags()]
+    public enum OrderOptionType
+    {
+        /// <summary>
+        /// None.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Client order disallowed.
+        /// </summary>
+        DisallowAllowOrder = 1,
+
+        /// <summary>
+        /// Disallow ability of order for non users.
+        /// </summary>
+        RestrictNonCustomers = 2,
+
+        /// <summary>
+        /// Restricts product sale.
+        /// </summary>
+        RestrictSale = 4,
+
+        /// <summary>
+        /// If the product is service.
+        /// </summary>
+        IsService = 8,
+
+        /// <summary>
+        /// If the product is marked product.
+        /// </summary>
+        IsMarkedProduct = 16,
+
+        /// <summary>
+        /// Requires preparation.
+        /// </summary>
+        RequiresPreparation = 32,
+
+        /// <summary>
+        /// If the product is excise product.
+        /// </summary>
+        IsExcise = 64,
+    }
+}
