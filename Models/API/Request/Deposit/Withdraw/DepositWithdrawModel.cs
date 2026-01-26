8b95d296a28031936925ace6953a5bee2d8d48f2
Oleg Savelos <olegsavelos@gmail.com>
2026-01-26 13:12:28 +0200

    withdraw functionality.

---
+++ Models/API/Request/Deposit/Withdraw/DepositWithdrawModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Deposit withdrawal model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class DepositWithdrawModel : IWebApiModel
+    {
+        /// <summary>
+        /// Withdraw amount.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public required decimal Amount { get; init; }
+
+        /// <summary>
+        /// Refund payment method id.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public required int RefundPaymentMethodId { get; init; }
+    }
+}
---
+++ Models/API/Request/Deposit/Withdraw/DepositWithdrawPaymentsStateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Deposit withdraw payments model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class DepositWithdrawPaymentsStateModel : IWebApiModel
+    {
+        /// <summary>
+        /// Allowed payment methods.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public required IEnumerable<int> AllowedPaymentMethods { get; init; }
+
+        /// <summary>
+        /// Maximum withdraw amount.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public decimal MaximumAmount { get; init; }
+    }
+}
