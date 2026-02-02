1e8e7e7dec43428429d4b5ceebd84660d2748aa5
Oleg Savelos <olegsavelos@gmail.com>
2026-02-02 16:40:13 +0200

    updates.

---
+++ Models/API/Request/User/Ban/UserMemberBanReasonModel.cs
@@ -0,0 +1,27 @@
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// User member ban reason model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class UserMemberBanReasonModel : IWebApiModel , IModelIntIdentifier
+    {
+        /// <inheritdoc/>
+        [MessagePack.Key(0)]
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public required string Name { get; init; }
+
+        /// <summary>
+        /// Description.
+        /// </summary>
+        [MessagePack.Key(2)]
+        public string? Description { get; init; }
+    }
+}
