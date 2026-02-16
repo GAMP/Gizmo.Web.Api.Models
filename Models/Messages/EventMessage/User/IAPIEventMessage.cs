b57a0e032268fa3abf966abb783bb6f18e8ec61a
Oleg Savelos <olegsavelos@gmail.com>
2026-02-16 13:16:37 +0200

    fix.

--- Models/Messages/EventMessage/User/IAPIEventMessage.cs
+++ Models/Messages/EventMessage/User/IAPIEventMessage.cs
@@ -14,9 +14,9 @@ namespace Gizmo.Web.Api.Messaging
     [Union(309, typeof(UserRenamedEventMessage))]
     [Union(310, typeof(UserPasswordChangedEventMessage))]
     [Union(311, typeof(UserBillingOptionsChangedEventMessage))]
-    [Union(311, typeof(UserNoteAddedEventMessage))]
-    [Union(312, typeof(UserNoteUpdatedEventMessage))]
-    [Union(313, typeof(UserNoteDeletedEventMessage))]
+    [Union(312, typeof(UserNoteAddedEventMessage))]
+    [Union(313, typeof(UserNoteUpdatedEventMessage))]
+    [Union(314, typeof(UserNoteDeletedEventMessage))]
     public partial interface IAPIEventMessage
     {
     }
