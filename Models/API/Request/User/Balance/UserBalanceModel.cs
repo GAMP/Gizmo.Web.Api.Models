8878a12429a57830308a9a15dfd9db33e2eaef98
Oleg Savelos <olegsavelos@gmail.com>
2026-02-16 16:22:40 +0200

    updates.

--- Models/API/Request/User/Balance/UserBalanceModel.cs
+++ Models/API/Request/User/Balance/UserBalanceModel.cs
@@ -1,71 +1,68 @@
-﻿using System;
-using MessagePack;
-
-namespace Gizmo.Web.Api.Models
+﻿namespace Gizmo.Web.Api.Models
 {
-    /// <inheritdoc/>
-    [Serializable, MessagePackObject]
+    /// <inheritdoc cref="IUserBalanceModel"></inheritdoc>
+    [MessagePack.MessagePackObject]
     public sealed class UserBalanceModel : IUserBalanceModel
     {
         /// <inheritdoc/>
-        [Key(0)]
+        [MessagePack.Key(0)]
         public double? AvailableCreditedTime { get; init; }
 
         /// <inheritdoc/>
-        [Key(1)]
+        [MessagePack.Key(1)]
         public double? AvailableTime { get; init; }
 
         /// <inheritdoc/>
-        [Key(2)]
+        [MessagePack.Key(2)]
         public decimal Deposits { get; init; }
 
         /// <inheritdoc/>
-        [Key(3)]
+        [MessagePack.Key(3)]
         public decimal OnInvoicedUsage { get; init; }
 
         /// <inheritdoc/>
-        [Key(4)]
+        [MessagePack.Key(4)]
         public decimal OnInvoices { get; init; }
 
         /// <inheritdoc/>
-        [Key(5)]
+        [MessagePack.Key(5)]
         public decimal OnUninvoicedUsage { get; init; }
 
         /// <inheritdoc/>
-        [Key(6)]
+        [MessagePack.Key(6)]
         public int Points { get; init; }
 
         /// <inheritdoc/>
-        [Key(7)]
+        [MessagePack.Key(7)]
         public double TimeFixed { get; init; }
 
         /// <inheritdoc/>
-        [Key(8)]
+        [MessagePack.Key(8)]
         public double TimeProduct { get; init; }
 
         /// <inheritdoc/>
-        [IgnoreMember()]
+        [MessagePack.IgnoreMember()]
         public decimal Balance
         {
             get { return Deposits - OnInvoices - OnUninvoicedUsage; }
         }
 
         /// <inheritdoc/>
-        [IgnoreMember()]
+        [MessagePack.IgnoreMember()]
         public double TimeProductBalance
         {
             get { return TimeProduct + TimeFixed; }
         }
 
         /// <inheritdoc/>
-        [IgnoreMember()]
+        [MessagePack.IgnoreMember()]
         public decimal UsageBalance
         {
             get { return OnInvoicedUsage + OnUninvoicedUsage; }
         }
 
         /// <inheritdoc/>
-        [IgnoreMember()]
+        [MessagePack.IgnoreMember()]
         public decimal TotalOutstanding
         {
             get { return OnInvoices + OnUninvoicedUsage; }
