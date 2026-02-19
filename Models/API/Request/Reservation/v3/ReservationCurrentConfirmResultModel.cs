01cdf071b57374a65377b378782d7123ee7bac9c
Oleg Savelos <olegsavelos@gmail.com>
2026-02-19 11:43:10 +0200

    new reservation models.

---
+++ Models/API/Request/Reservation/v3/ReservationCurrentConfirmResultModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Reservation confirmation result model.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class ReservationCurrentConfirmResultModel : IWebApiModel
+    {
+        /// <summary>
+        /// Confirmation result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ReservationCurrentConfirmResult Result { get; init; }
+    }
+}
---
+++ Models/API/Request/Reservation/v3/ReservationCurrentConfirmedResultModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Current reservation confirmed result model.
+    /// </summary>
+    [MessagePack.MessagePackObject]
+    public sealed class ReservationCurrentConfirmedResultModel : IWebApiModel
+    {
+        /// <summary>
+        /// Confirmed result.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public ReservationCurrentConfirmedResult Result { get; init; }
+    }
+}
