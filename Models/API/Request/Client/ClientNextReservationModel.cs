0447745996f9c008c82f80ff3bb36a5230259ddb
Oleg Savelos <olegsavelos@gmail.com>
2026-02-24 13:22:11 +0200

    add new reservation information properties.

--- Models/API/Request/Client/ClientNextReservationModel.cs
+++ Models/API/Request/Client/ClientNextReservationModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace Gizmo.Web.Api.Models
 {
@@ -30,5 +32,25 @@ namespace Gizmo.Web.Api.Models
         [NotNullIfNotNull(nameof(NextReservationId))]
         [MessagePack.Key(4)]
         public ReservationPaymentStatus? PaymentStatus { get; init; }
+
+        /// <summary>
+        /// Total amount of reservation.
+        /// </summary>
+        [NotNullIfNotNull(nameof(NextReservationId))]
+        [MessagePack.Key(5)]
+        public decimal Total { get; init; }
+
+        /// <summary>
+        /// Total outstanding amount.
+        /// </summary>
+        [NotNullIfNotNull(nameof(NextReservationId))]
+        [MessagePack.Key(6)]
+        public decimal Outstanding { get; init; }
+
+        /// <summary>
+        /// Reservation hosts.
+        /// </summary>
+        [MessagePack.Key(7)]
+        public IEnumerable<ReservationInfoHostModel> Hosts { get; set; } = Enumerable.Empty<ReservationInfoHostModel>();
     }
 }
---
+++ Models/API/Request/Client/ReservationInfoHostModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Reservation info host model.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public sealed class ReservationInfoHostModel : IWebApiModel
+    {
+        /// <summary>
+        /// Host number.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public int HostNumber { get; init; }
+
+        /// <summary>
+        /// Host name.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public required string HostName { get; init; }
+    }
+}
