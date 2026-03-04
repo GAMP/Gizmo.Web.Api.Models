b713d210ba8c1ef6979410bf22af8a60f68706d3
Oleg Savelos <olegsavelos@gmail.com>
2026-03-04 13:04:22 +0200

    missing class.

--- Models/API/Request/FileSystem/DirectoryEnumerateParametersModel.cs
+++ Models/API/Request/FileSystem/DirectoryEnumerateParametersModel.cs
@@ -1,10 +1,12 @@
-﻿namespace Gizmo.Web.Api.Models
+﻿using Gizmo.Web.Api.Models.Abstractions;
+
+namespace Gizmo.Web.Api.Models
 {
     /// <summary>
     /// Directory enumeration parameters model.
     /// </summary>
     [MessagePack.MessagePackObject()]
-    public sealed class DirectoryEnumerateParametersModel 
+    public sealed class DirectoryEnumerateParametersModel : IUriParametersQuery
     {
         /// <summary>
         /// Enumeration path.
