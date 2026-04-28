85f36642707bacc4520449290756a3a775030609
mrdpod <mr.d.pod@mail.ru>
2026-04-28 13:11:03 +0300

    feat: add TokenCheckModel and TokenConfirmedResultModel

---
+++ Models/API/Result/Verification/TokenConfirmedResultModel.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+
+namespace Gizmo.Web.Api.Models
+{
+    /// <summary>
+    /// Token confirmation state result.
+    /// </summary>
+    [MessagePackObject]
+    public sealed class TokenConfirmedResultModel
+    {
+        /// <summary>
+        /// Indicates whether token has been confirmed.
+        /// </summary>
+        [Key(0)]
+        public bool IsConfirmed { get; set; }
+
+        /// <summary>
+        /// Confirmed phone number.
+        /// </summary>
+        [Key(1)]
+        public string? Phone { get; set; }
+    }
+}
