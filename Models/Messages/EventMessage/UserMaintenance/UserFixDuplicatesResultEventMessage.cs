a0616e5dfbf345a9dc52e470f75957415137a954
Andrei Pestunov <masterlifting@gmail.com>
2026-05-12 08:36:58 +0200

    Refactor user fix duplicates event message property names for consistency

--- Models/Messages/EventMessage/UserMaintenance/UserFixDuplicatesResultEventMessage.cs
+++ Models/Messages/EventMessage/UserMaintenance/UserFixDuplicatesResultEventMessage.cs
@@ -14,30 +14,30 @@ namespace Gizmo.Web.Api.Messaging
         /// Processed user count.
         /// </summary>
         [Key(5)]
-        public int ProcessedUserCount { get; init; }
+        public int ProcessedUsersCount { get; init; }
 
         /// <summary>
         /// Count of users whose phone fields need normalization updates.
         /// </summary>
         [Key(6)]
-        public int NormalizedPhoneUserCount { get; init; }
+        public int NormalizedPhonesCount { get; init; }
 
         /// <summary>
         /// Count of users with invalid phone fields.
         /// </summary>
         [Key(7)]
-        public int InvalidPhoneUserCount { get; init; }
+        public int InvalidPhonesCount { get; init; }
 
         /// <summary>
         /// Count of duplicate normalized mobile phone groups.
         /// </summary>
         [Key(8)]
-        public int DuplicateGroupCount { get; init; }
+        public int MergedPhonesCount { get; init; }
 
         /// <summary>
         /// Count of users that belong to duplicate normalized mobile phone groups.
         /// </summary>
         [Key(9)]
-        public int DuplicateUserCount { get; init; }
+        public int MergeErrorsCount { get; init; }
     }
 }
