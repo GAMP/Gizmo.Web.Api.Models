8da304738c359dc338f13f55ea0b721384dfebb7
mrdpod <mr.d.pod@mail.ru>
2026-09-23 15:55:57 +0300

    feat: ladder level in profile header and top bar, ladder tech debt

--- Models/API/Request/Achievement/UserAchievementLadderEventModel.cs
+++ Models/API/Request/Achievement/UserAchievementLadderEventModel.cs
@@ -21,7 +21,6 @@ namespace Gizmo.Web.Api.Models
         /// UTC event time.
         /// </summary>
         [MessagePack.Key(1)]
-        [Sortable]
         public DateTime CreatedTime { get; init; }
 
         /// <summary>
